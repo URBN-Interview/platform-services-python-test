5182535b9d885fa4f3d6766643b71fb603edfc84
Jean <jean@ticketleap.com>
2019-12-15 16:01:12 -0500

    Add User Rewards Section

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -6,6 +6,8 @@ class RewardsServiceClient:
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
         self.order_url = "http://rewardsservice:7050/manage_customer"
+        self.customer_url = "http://rewardsservice:7050/get_customer/"
+        self.all_customers_url = "http://rewardsservice:7050/get_all_customers"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
@@ -13,4 +15,13 @@ class RewardsServiceClient:
 
     def add_order(self, email_address, order_total):
         response = requests.post(self.order_url, data={'email_address': email_address, 'order_total': order_total})
+        return response.json()
+
+    def get_customer(self, email_search):
+        url = self.customer_url + email_search
+        response = requests.get(url)
+        return response.json()
+
+    def get_customers(self):
+        response = requests.get(self.all_customers_url)
         return response.json()
