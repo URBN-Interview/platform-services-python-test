5629856337ae6fdca893c6c5ee1a20ce6706f507
Eric Trudnak <erictrudnak@gmail.com>
2019-12-28 23:18:32 -0500

    Create single_customer_handler.py

---
+++ source/RewardsService/rewardsservice/handlers/single_customer_handler.py
@@ -0,0 +1,32 @@
+import json
+import tornado.web
+
+from pymongo import MongoClient
+
+
+class SingleCustomerHandler(tornado.web.RequestHandler):
+
+    email = None
+
+    def post(self):
+        """Get customers email address from field entry"""
+        try:
+            SingleCustomerHandler.email = self.get_arguement("email address")
+        except:
+            print("Error getting email address")
+
+    def get(self):
+        """Search for customers info in database and return it"""
+        try:
+            client = MongoClient("mongodb", 27017)
+            db = client["Rewards"]
+            customers = list(db.customers.find({}, {"_id": 0}))
+
+            for x in customers:
+                for key, value in x.iteritems():
+                    if key == 'email' and value == SingleCustomerHandler.email:
+                        customer_info = x
+                        return customer_info
+
+        except:
+            print("Error finding customer info")
