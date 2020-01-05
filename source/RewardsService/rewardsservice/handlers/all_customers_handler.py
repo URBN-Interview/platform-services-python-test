e76b3824e711b97052dbb0a4d838bd9c64e83044
Eric Trudnak <erictrudnak@gmail.com>
2020-01-05 15:34:06 -0500

    Update all_customers_handler.py

--- source/RewardsService/rewardsservice/handlers/all_customers_handler.py
+++ source/RewardsService/rewardsservice/handlers/all_customers_handler.py
@@ -1,18 +1,20 @@
 import tornado.web
+import json
 
 from pymongo import MongoClient
+from tornado.gen import coroutine
 
 
-class SingleCustomerHandler(tornado.web.RequestHandler):
+class AllCustomersHandler(tornado.web.RequestHandler):
     """Returns all customers reward info"""
 
+    @coroutine
     def get(self):
         """Accesses and returns list of all customers rewards info"""
         try:
             client = MongoClient("mongodb", 27017)
             db = client["Rewards"]
             customers = list(db.customers.find({}, {"_id": 0}))
-            return customers
-
+            self.write(json.dumps(customers))
         except:
             print("Error finding all customers info")
