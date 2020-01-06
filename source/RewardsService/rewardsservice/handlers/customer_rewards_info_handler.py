747bd93d042a6413e723c1847be848dbaef382fe
Eric Trudnak <erictrudnak@gmail.com>
2020-01-05 20:32:31 -0500

    Update customer_rewards_info_handler.py

--- source/RewardsService/rewardsservice/handlers/customer_rewards_info_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_rewards_info_handler.py
@@ -1,7 +1,6 @@
 import tornado.web
 import math
-
-from pymongo import MongoClient
+import pymongo
 
 
 class CustomerInfoHandler(tornado.web.RequestHandler):
@@ -11,99 +10,67 @@ class CustomerInfoHandler(tornado.web.RequestHandler):
     email = None
     points = None
     rewards_tier = None
-    next_tier = None
 
     @staticmethod
     def create_dictionary(*args):
         return dict((k, eval(k)) for k in args)
-
 
     def post(self):
         """Gets customer field input"""
         try:
             CustomerInfoHandler.email = self.get_arguement('order_email')
             tmp_points = self.get_arguement('order_total')
-            CustomerInfoHandler.points = int(tmp_points) # round down change
+            CustomerInfoHandler.points = int(tmp_points)  # round down change
 
             # Customer can't exceed 1000 points
             if CustomerInfoHandler.points > 1000:
                 CustomerInfoHandler.points = 1000
-        except TyperError:
-            print("A TypeError occurred")
         except:
             print("Error retrieving form data")
-
 
     def get(self):
         """Get the list of reward tiers from DB and find/store appropriate one"""
         try:
             client = MongoClient("mongodb", 27017)
             db = client["Rewards"]
-            rewards = list(db.rewards.find({}, {"_id": 0}))
-            customers = list(db.customers.find({}, {"_id": 0}))
-            tier_found = False
-            next_tier_found = False
-            email_found = False
+            customers = db["customers"]
+            rewards = db["rewards"]
 
-            # Checks if customer already exists in Rewards DB and if so adds their points to this order total
-            # Once, customer email is found, it continues onto next inner loop (next key) and adds the pre-existing points
-            for y in customers:
-                for key, value in y.iteritems():
-                    if key == 'email' and value == CustomerInfoHandler.email:
-                        email_found = True
-                        continue
-                    if email_found is True:
-                        CustomerInfoHandler.points += value
-                        break
-                if email_found is True:
-                    break
+            # find customer data if exists, add points if it does
+            customer_found = customers.find({"email": CustomerInfoHandler.email}, {"_id": 0})
+            if customer_found is not None:
+                x = json.loads(customer_found)
+                CustomerInfoHandler.points += x["points"]
+                if CustomerInfoHandler.points > 1000:
+                    CustomerInfoHandler.points = 1000
 
             # Round points down to find appropriate rewards tier
             tmp_points = int(math.floor(CustomerInfoHandler.points / 100)) * 100
 
-            # Find correct rewards tier via points
-            # Loops through tiers to find correct one, and breaks out of inner loop if found
-            # On next loop of tiers, it sets the next_tier value and breaks out of inner loop
-            # and with both found, the if statement breaks out of outer loop
-            for x in rewards:
-                for key, value in x.iteritems():
-                    if key == 'points' and value == tmp_points:
-                        CustomerInfoHandler.rewards_tier = x
-                        tier_found = True
-                        break
-                    elif tier_found is True:
-                        CustomerInfoHandler.next_tier = x
-                        next_tier_found = True
-                        break
-                if tier_found is True and next_tier_found is True:       # Break outer loop if correct info found
-                    break
-        except TyperError:
-            print("A TypeError occurred")
+            # find appropriate rewards tier in collection
+            CustomerInfoHandler.rewards_tier = rewards.find({"points": tmp_points}, {"_id": 0})
         except:
             print("Error finding correct tier")
-
 
     def put(self):
         """Use data gathered above to input customer reward data into DB"""
         try:
-            for key, value in CustomerInfoHandler.rewards_tier:     # For loop to get reward name and tier name
-                if key == 'rewardName':
-                    rewardName = value
-                elif key == 'tier':
-                    tier = value
+            client = MongoClient("mongodb", 27017)
+            db = client["Rewards"]
+            customers = db["customers"]
 
-            # If customers points are less than 1000:
-            # for loop to get next reward name, next tier name, and next points
+            x = json.loads(CustomerInfoHandler.rewards_tier)
+            rewardName = x["rewardName"]
+            tier = x["tier"]
+
+            # Get next tier data
             if CustomerInfoHandler.points != 1000:
-                for key, value in CustomerInfoHandler.next_tier:
-                    if key == 'rewardName':
-                        nextRewardName = value
-                    elif key == 'tier':
-                        nextTier = value
-                    elif key == 'points':
-                        nextPoints = value
-                    nextTierProgression = str(int((CustomerInfoHandler.points / nextPoints) * 100)) + "%"
-            else:                               # else the customer has reached the max tier
+                nextTier = chr(ord(tier) + 1)
+                y = json.loads(customers.find({"tier": nextTier}))
+                nextRewardName = y["rewardName"]
+                nextPoints = y["nextPoints"]
+                nextTierProgression = str(int((CustomerInfoHandler.points / nextPoints) * 100)) + "%"
+            else:  # else the customer has reached the max tier
                 nextRewardName = "N/A"
                 nextTier = "N/A"
                 nextTierProgression = "N/A"
@@ -112,17 +79,12 @@ class CustomerInfoHandler(tornado.web.RequestHandler):
             email = CustomerInfoHandler.email
             points = CustomerInfoHandler.points
             customer_info = self.create_dictionary(email, points, tier,
-                                                   rewardName,nextTier, nextRewardName, nextTierProgression)
+                                                   rewardName, nextTier, nextRewardName, nextTierProgression)
 
             # Insert customer info into Customer DB collection
-            client = MongoClient("mongodb", 27017)
-            db = client["Rewards"]
-            coll = db["customers"]
-            myquery = self.create_dictionary(email)     # Find previous entry and delete it
-            coll.delete_one(myquery)
+            myQuery = self.create_dictionary(email)  # Find previous entry and delete it
+            coll.delete_one(myQuery)
             coll.insert_one(customer_info)
 
-        except TyperError:
-            print("A TypeError occurred")
         except:
             print("Error inserting customer info")
