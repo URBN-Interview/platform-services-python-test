e9f684f692b8bf71d6f0c9b11452d64439d16c1e
Eric Trudnak <erictrudnak@gmail.com>
2020-01-07 21:38:26 -0500

    Update url_patterns.py

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,9 +1,11 @@
 from handlers.rewards_handler import RewardsHandler
 from handlers.customer_rewards_info_handler import CustomerInfoHandler
 from handlers.all_customers_handler import AllCustomersHandler
+from handlers.single_customer_handler import SingleCustomerHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
     (r'/addorder', CustomerInfoHandler),
     (r'/allcustomerpoints', AllCustomersHandler),
+    (r'/rewardssearch', SingleCustomerHandler),
 ]
