b95d1df796366d0eac39ca9d35c58b25d30d69c4
xalxnder <xalexander51@gmail.com>
2020-01-23 18:36:48 -0500

    Setting up route for first endpoint

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,7 @@
-from handlers.rewards_handler import RewardsHandler
+# Importing all of the handlers
+from handlers.rewards_handler import *
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/order_data', OrderDatatHandler),
 ]
