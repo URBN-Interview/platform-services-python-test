369dcdfe0f3b8db3518d86c7d014bacb603c928a
xalxnder <xalexander51@gmail.com>
2020-01-25 08:08:44 -0500

    Add argument methods to accept customer email and order total

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -15,32 +15,13 @@ class RewardsHandler(tornado.web.RequestHandler):
 
 
 class OrderDatatHandler(tornado.web.RequestHandler):
+    """Endpoint which accepts a customers email adress, and order total, and stores
+    the their rewards data based on the amount of points they have. """
     @coroutine
     def get(self):
         client = MongoClient("mongodb", 27017)
         db = client["Rewards"]
         rewards = list(db.rewards.find({}, {"_id": 0}))
+        # Using the argument method to accept customer email & order total.
         email_address = self.get_argument("email_address")
         order_total = self.get_argument("order_total")
-        points = order_total * 1
-        for i in range(len(rewards)):
-            max = int(rewards[i]['points'])
-            min = int(rewards[i]['points']-100)
-            if int(order_total) in range(min, max):
-                current_reward = int(
-                    rewards[i]['rewardName'].split()[0].strip("%"))
-                next_reward = int(
-                    rewards[i+1]['rewardName'].split()[0].strip("%"))
-                next_reward_progress = next_reward - current_reward
-                rewards_data = [
-                    {'emailAddress': email_address,
-                     'rewardPoints': points,
-                     'rewardTier': rewards[i]['tier'],
-                     'rewardTierName': rewards[i]['rewardName'],
-                     'nextRewardTier': rewards[i+1]['tier'],
-                     'nextRewardTierName': rewards[i+1]['rewardName'],
-                     'nextRewardTierProgress': next_reward_progress/100,
-                     }
-                ]
-                break
-        db.rewards_data.insert_many(rewards_data)
