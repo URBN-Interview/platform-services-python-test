518ffafd759de9a5c8f00b53e6c1bbaf73b64b17
xalxnder <xalexander51@gmail.com>
2020-01-27 11:36:24 -0500

    Add the send_order_data_url variable
    Add the send_order_data_url variable to the constructor method
    which contains the endpoint that allows customers to submit an email
    address and order total

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -5,6 +5,7 @@ class RewardsServiceClient:
 
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
+        self.send_order_data_url = "http://rewardsservice:7050/order_data?email_address="
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
