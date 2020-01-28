6c6320a36e27991695aaff344862c000d8a356e2
xalxnder <xalexander51@gmail.com>
2020-01-28 08:49:19 -0500

    Add post method
    Add post method that contains a conditional which states IF the value of the form  search button is
    "Search", use the search_rewards_data_url endpoint to search the DB collection for the document matching the supplied
    email address,  and set the context to rewards_data and all_rewards_data. Otherwise,
    use the send_order_data_url endpoint to accept an email address and order total to send to the DB collection.

--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -25,3 +25,27 @@ class RewardsView(TemplateView):
             self.template_name,
             context
         )
+
+    """Setting up a post method that uses the search_rewards_data_url endpoint IF the value of the form button is "Search", but otherwise
+        uses the send_order_data_url url.
+    """
+
+    def post(self, request, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.POST['action'] == 'Search':
+            searchEmail = request.POST.get("searchEmailAddress")
+            all_rewards_data = self.rewards_service_client.search_rewards_data(
+                searchEmail)
+            context['rewards_data'] = self.rewards_data
+            context['all_rewards_data'] = all_rewards_data
+            return TemplateResponse(request, self.template_name, context)
+        else:
+            email = request.POST.get("emailAddress")
+            total = request.POST.get("orderTotal")
+            get_data = self.rewards_service_client.send_order_data(
+                email, total)
+            context['rewards_data'] = self.rewards_data
+            return TemplateResponse(
+                request,
+                self.template_name, context
+            )
