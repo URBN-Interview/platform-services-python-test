f2ba90945f846bd997a952a14e67e0442e32fbc1
Jeff Li <jeffli95@gmail.com>
2020-02-05 17:28:18 -0500

    Implement get all users and their data

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -5,12 +5,17 @@ class RewardsServiceClient:
 
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
-        self.user_url = "http://rewardsservice:7050/customers"
+        self.users_url = "http://rewardsservice:7050/allcustomers"
+        self.order_url = "http://rewardsservice:7050/customers"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
         return response.json()
 
     def get_user(self, email_address):
-        response = requests.get(self.user_url, {"email-address": email_address})
+        response = requests.get(self.order_url, {"email-address": email_address})
+        return response.json()
+
+    def get_users(self):
+        response = requests.get(self.users_url)
         return response.json()
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -6,6 +6,7 @@ from django.views.generic.base import TemplateView
 from rewards.clients.rewards_service_client import RewardsServiceClient
 
 from rewards.forms import SearchUserForm
+from rewards.forms import AddOrderForm
 
 
 class RewardsView(TemplateView):
@@ -14,36 +15,43 @@ class RewardsView(TemplateView):
     def __init__(self, logger=logging.getLogger(__name__), rewards_service_client=RewardsServiceClient()):
         self.logger = logger
         self.rewards_service_client = rewards_service_client
-
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-
-        rewards_data = self.rewards_service_client.get_rewards()
-        context['rewards_data'] = rewards_data
-
-        return TemplateResponse(
-            request,
-            self.template_name,
-            context
-        )
-
-class SearchUserView(TemplateView):
-    template_name = 'index.html'
-
-    def __init__(self, logger=logging.getLogger(__name__), rewards_service_client=RewardsServiceClient()):
-        self.logger = logger
-        self.rewards_service_client = rewards_service_client
+        self.rewards_data = rewards_service_client.get_rewards()
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         if request.method == 'GET':
-            form = SearchUserForm(request.GET, user=request.user)
+            form = SearchUserForm(request.GET)
             if form.is_valid():
                 user_data = self.rewards_service_client.get_user(form.cleaned_data['email_address'])
-                context['user_data'] = user_data
+                context['users_data'] = user_data
+                context['rewards_data'] = self.rewards_data
+            else:
+                self.get_data(context)
+        else:
+            self.get_data(context)
         return TemplateResponse(
             request,
             self.template_name,
             context
         )
 
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.method == 'POST':
+            form = AddOrderForm(request.POST)
+            if form.is_valid():
+                self.rewards_service_client.add_order(form.cleaned_data['email_address_order'],
+                                                      form.cleaned_data['order_total'])
+                context['order_status'] = "ORDER COMPLETE"
+            else:
+                context['order_status'] = "ORDER FAILED"
+        self.get_data(context)
+        return TemplateResponse(
+            request,
+            self.template_name,
+            context
+        )
+
+    def get_data(self, context):
+        context['rewards_data'] = self.rewards_data
+        context['users_data'] = self.rewards_service_client.get_users()
