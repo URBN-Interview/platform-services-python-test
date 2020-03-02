ca5c356a868e884e63007b24488d1b00268a5744
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-02 14:46:03 -0500

    Add mongodb integration to customer post request

--- source/RewardsService/rewardsservice/handlers/customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_handler.py
@@ -7,14 +7,20 @@ from tornado.gen import coroutine
 
 
 class CustomerHandler(tornado.web.RequestHandler):
-    
+    collectionName = 'Customers'
 
     @coroutine
     def post(self):
+        client = MongoClient("mongodb", 27017)
+        db = client[self.collectionName]
+        validation = Validaton()
+
         email = str(self.get_argument('email', ''))
         orderTotal = str(self.get_argument('orderTotal', ''))
-        
-        Validaton().emailValidation(email).currencyValidation(orderTotal).validate()
+
+        validation.emailValidation(email).currencyValidation(orderTotal).validate()
+        customer = db.customers.insert({"email": email, "orderTotal": orderTotal})
+
         self.write({"email": email, "orderTotal": orderTotal})
 
 
