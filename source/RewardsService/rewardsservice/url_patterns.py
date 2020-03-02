c1298a7e040f17fdc63c0fd2677471a2d79d122b
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-02 17:45:46 -0500

    update url patterns to handler logic

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,7 +1,11 @@
 from handlers.rewards_handler import RewardsHandler
-from handlers.customers_handler import CustomersHandler
+from handlers.all_customers_handler import AllCustomersHandler
+from handlers.customer_handler import CustomerHandler
+from handlers.order_handler import OrderHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
-    (r'/customers', CustomersHandler)
+    (r'/customers', AllCustomersHandler),
+    (r'/customer', CustomerHandler),
+    (r'/order', OrderHandler)
 ]
