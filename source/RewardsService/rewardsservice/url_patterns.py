37f07ec555b4c8fa9d7e450f102bda2220d84db2
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-02 17:19:04 -0500

    Update customer import and reference

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,7 +1,7 @@
 from handlers.rewards_handler import RewardsHandler
-from handlers.customer_handler import CustomersHandler
+from handlers.customers_handler import CustomersHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
-    (r'/customers', CustomerHandler)
+    (r'/customers', CustomersHandler)
 ]
