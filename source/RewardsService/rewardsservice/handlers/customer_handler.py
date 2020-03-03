98eab8c7c14fa07a69fbef49348a1ac208fbaf28
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-03 13:29:21 -0500

    Add error handling to customer handler

--- source/RewardsService/rewardsservice/handlers/customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_handler.py
@@ -1,21 +1,32 @@
 import tornado.web
 import json
 
-from util.validation import Validaton
 from pymongo import MongoClient
 from tornado.gen import coroutine
 from tornado.options import options
 
+from util.validation import Validaton
+
 class CustomerHandler(tornado.web.RequestHandler):
     collectionName = 'Customers'
-    
+    error = None
+
     @coroutine
     def get(self):
         client = MongoClient(options.mongodb_host)
         db = client[self.collectionName]
         
         email = str(self.get_argument('email', ''))
-        Validaton().emailValidation(email).validate()
+        validateError = Validaton().emailValidation(email).validate()
+        if validateError:
+            self.error = validateError
+            raise Exception(self.error.type)
 
         customers = db.customers.find_one({"email": email}, {"_id": 0})
         self.write(json.dumps(customers))
+
+    def write_error(self, status_code, **kwargs):
+        if status_code == 500 and self.error:
+            self.write({"type" : self.error.type, "context": self.error.context})
+        elif status_code == 500:
+            self.write({'message: Unknown Error'})
