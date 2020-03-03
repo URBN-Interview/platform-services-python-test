35fb6302d0176cd40707c26d92854899f66ebed2
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-03 01:00:18 -0500

    Added current and next reward logic and tier progress

--- source/RewardsService/rewardsservice/handlers/order_handler.py
+++ source/RewardsService/rewardsservice/handlers/order_handler.py
@@ -1,24 +1,55 @@
 import tornado.web
 import json
+import math
 
 from util.validation import Validaton
 from pymongo import MongoClient
 from tornado.gen import coroutine
 from tornado.options import options
+from model.customer import Customer
 
 class OrderHandler(tornado.web.RequestHandler):
-    collectionName = 'Customers'
+    customerClient = 'Customers'
+    rewardsClient = 'Rewards'
 
     @coroutine
     def post(self):
         client = MongoClient(options.mongodb_host)
-        db = client[self.collectionName]
-        validation = Validaton()
+        customerDb = client[self.customerClient]
+        rewardsDb = client[self.rewardsClient]
 
         email = str(self.get_argument('email', ''))
         orderTotal = str(self.get_argument('orderTotal', ''))
+        Validaton().emailValidation(email).currencyValidation(orderTotal).validate()
+        
+        customerExist = customerDb.customers.find_one({'email': email}, {'_id': 0})
+        if(customerExist):
+            raise Exception('Customer already exist')
 
-        validation.emailValidation(email).currencyValidation(orderTotal).validate()
-        customer = db.customers.insert({"email": email, "orderTotal": orderTotal})
+        customer = Customer(email, float(orderTotal))
+        dallor = int(orderTotal.split('.')[0])
+        currentReward = None
+        nextReward = None
 
-        self.write({"email": email, "orderTotal": orderTotal})
+        for reward in list(rewardsDb.rewards.find({}, {'_id': 0})):
+            if(dallor < reward['points']):
+                nextReward = reward
+                break
+
+            currentReward = reward
+
+        if(currentReward):
+            customer.setReward(currentReward['tier'], currentReward['rewardName'])
+
+        if(nextReward):
+            customer.setNextReward(nextReward['tier'], nextReward['rewardName'])
+
+        if(nextReward):
+            currentRewardPoints = 0
+            if(currentReward):
+                currentRewardPoints = currentReward['points']
+            customer.tierProgress = (dallor - currentRewardPoints)/(nextReward['points'] - currentRewardPoints)
+
+        customerDb.customers.insert({'email': customer.email, 'orderTotal': customer.orderTotal, 'rewardTier': customer.rewardTier, 'rewardName': customer.rewardName, 'nextRewardTier': customer.nextRewardTier, 'nextRewardName': customer.nextRewardName, 'nextRewardTierProgress': customer.tierProgress})
+        createdCustomer = customerDb.customers.find_one({'email': email}, {'_id': 0})
+        self.write(json.dumps(createdCustomer))
