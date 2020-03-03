76d87d4c885a924ba4457e88d43d371a8fd8ac13
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-03 13:28:51 -0500

    Add state pattern server error classes

---
+++ source/RewardsService/rewardsservice/util/server_error.py
@@ -0,0 +1,12 @@
+class ServerError():
+    def __init__ (self, type, context):
+        self.type = type
+        self.context = context
+    
+class ValidationError(ServerError):
+    def __init__(self, context):
+        super().__init__('ValidationError', context)
+
+class DatabaseError(ServerError):
+    def __init__(self, context):
+        super().__init__('DatabaseError', context)
