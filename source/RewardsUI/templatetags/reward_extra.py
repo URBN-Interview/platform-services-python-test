95f9b520531e184cf950627886b92c0cf66ae43b
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-04 18:53:35 -0500

    Add floor on mutiple filter

--- source/RewardsUI/templatetags/reward_extra.py
+++ source/RewardsUI/templatetags/reward_extra.py
@@ -1,7 +1,8 @@
+import math
 from django import template
 
 register = template.Library()
 
 @register.filter(name="mutiple")
 def mutiple(n1, n2):
-    return n1 * n2
+    return math.floor(n1 * n2)
