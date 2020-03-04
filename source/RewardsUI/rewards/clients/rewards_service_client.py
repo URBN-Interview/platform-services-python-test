4f9246f4a80ca2f8de144f7b14dedf090f6f3944
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-04 16:39:24 -0500

    Add customer data to template

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -4,8 +4,14 @@ import requests
 class RewardsServiceClient:
 
     def __init__(self):
-        self.rewards_url = "http://rewardsservice:7050/rewards"
+        self.url = "http://rewardsservice:7050/"
+        self.rewards_endpoint = "rewards"
+        self.all_customers_endpoint = "customers"
 
     def get_rewards(self):
-        response = requests.get(self.rewards_url)
+        response = requests.get(self.url + self.rewards_endpoint)
+        return response.json()
+
+    def get_customers(self):
+        response = requests.get(self.url + self.all_customers_endpoint)
         return response.json()
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -4,7 +4,7 @@ from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
 
 from rewards.clients.rewards_service_client import RewardsServiceClient
-
+from .forms import EmailForm, OrderForm
 
 class RewardsView(TemplateView):
     template_name = 'index.html'
@@ -17,10 +17,40 @@ class RewardsView(TemplateView):
         context = self.get_context_data(**kwargs)
 
         rewards_data = self.rewards_service_client.get_rewards()
+        customer_data = self.rewards_service_client.get_customers()
+
         context['rewards_data'] = rewards_data
+        context['customer_data'] = customer_data
+        if request.method == 'GET':
+            form = EmailForm(request.GET)
+            if form.is_valid():
+                print(form.cleaned_data['email'])
 
         return TemplateResponse(
             request,
             self.template_name,
             context
         )
+    
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        rewards_data = self.rewards_service_client.get_rewards()
+        customer_data = self.rewards_service_client.get_customers()
+
+        print(customer_data)
+
+        context['rewards_data'] = rewards_data
+        context['customer_data'] = customer_data
+
+        if request.method == 'POST':
+            form = OrderForm(request.POST)
+            if form.is_valid():
+                print(form.cleaned_data)
+            
+        return TemplateResponse(
+            request,
+            self.template_name,
+            context
+        )
+        
