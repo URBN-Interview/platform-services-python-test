1bb803e26cbb84f4a3f1b3427bfa7a0f7c3c77c5
Chethtra Yen <chethtra.a.yen@gmail.com>
2020-03-04 17:37:34 -0500

    Added get customer endpoint

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -7,6 +7,7 @@ class RewardsServiceClient:
         self.url = "http://rewardsservice:7050/"
         self.rewards_endpoint = "rewards"
         self.all_customers_endpoint = "customers"
+        self.get_customer_endpoint = "customer"
 
     def get_rewards(self):
         response = requests.get(self.url + self.rewards_endpoint)
@@ -14,4 +15,8 @@ class RewardsServiceClient:
 
     def get_customers(self):
         response = requests.get(self.url + self.all_customers_endpoint)
+        return response.json()
+
+    def get_customer(self, email):
+        response = requests.get(self.url + self.get_customer_endpoint, {"email": email})
         return response.json()
