4f760b5ed02ef59b490f7f519875e9ff3f06ddc0
Kana-E <kana.eguchi@yahoo.com>
2020-08-13 19:12:17 -0400

    adds first model and registers to admin

--- source/RewardsUI/rewards/models.py
+++ source/RewardsUI/rewards/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class CustomerData(models.Model):
+class OrderData(models.Model):
       Email_Address = models.CharField(max_length=30)
+      Order_Total = models.IntegerField()
+
+
       Reward_Points = models.CharField(max_length=30)
       Reward_Tier = models.CharField(max_length=30)
       Reward_Tier_Name =  models.CharField(max_length=30)
       Next_Reward_Tier = models.CharField(max_length=30)
       Next_Reward_Tier Name = models.CharField(max_length=30)
       Next_Reward_Tier Progress = models.CharField(max_length=30)
+
+
+
+# class CustomerData(OrderData):
