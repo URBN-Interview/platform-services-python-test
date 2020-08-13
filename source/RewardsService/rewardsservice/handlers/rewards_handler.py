0c84d677cf8127154389442cb8ed4e795824c1dd
Kana-E <kana.eguchi@yahoo.com>
2020-08-12 22:47:10 -0400

    adds post req endpoint to rewards_handler

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -4,10 +4,12 @@ import tornado.web
 from pymongo import MongoClient
 from tornado.gen import coroutine
 
+from pymongo import MongoClient
+import json
 
 class RewardsHandler(tornado.web.RequestHandler):
 
-    @coroutine
+    @coroutine #async function
     def get(self):
         client = MongoClient("mongodb", 27017)
         db = client["Rewards"]
@@ -17,3 +19,17 @@ class RewardsHandler(tornado.web.RequestHandler):
 class Init(tornado.web.RequestHandler):
     def get(self):
         self.write({'message':'hello world'})
+
+class CustomerData(tornado.web.RequestHandler):
+
+    @coroutine #async function
+    def post(self):
+        # client = MongoClient("mongodb", 27017)
+        # db = client["Customer"]
+        # db.customers.insert({"Email": })
+        customerInfo = json.loads(self.request.body.decode('utf-8'))
+        self.write({'email': customerInfo['e-mail'], 'orderTotal': customerInfo['order-total'] }, )
+        # self.write({'orderTotal': customerInfo['order-total']})
+        # self.write({'order total': json.loads(self.request.body)})
+
+
--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,8 +1,9 @@
-from handlers.rewards_handler import RewardsHandler, Init
+from handlers.rewards_handler import RewardsHandler, Init, CustomerData
 
 #define endpoint settings
 url_patterns = [
     #test that this works
     (r'/', Init),
     (r'/rewards', RewardsHandler),
+    (r'/info', CustomerData),
 ]
