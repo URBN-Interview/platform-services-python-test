2b3595e872066a3d5c8069eca1ef095d6866204c
Kana-E <kana.eguchi@yahoo.com>
2020-08-13 16:44:29 -0400

    adds Customer Data Model

--- source/RewardsUI/rewards/models.py
+++ source/RewardsUI/rewards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomerData(models.Model):
+      Email_Address = models.CharField(max_length=30)
+      Reward_Points = models.CharField(max_length=30)
+      Reward_Tier = models.CharField(max_length=30)
+      Reward_Tier_Name =  models.CharField(max_length=30)
+      Next_Reward_Tier = models.CharField(max_length=30)
+      Next_Reward_Tier Name = models.CharField(max_length=30)
+      Next_Reward_Tier Progress = models.CharField(max_length=30)
