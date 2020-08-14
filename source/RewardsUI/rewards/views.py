bd008a51a9bef7bd1bc6477286c420d6b9a30c61
Kana-E <kana.eguchi@yahoo.com>
2020-08-14 12:03:19 -0400

    adds view class to show User Rewards
    get request defined in rewards_service_client.py

--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -4,10 +4,10 @@ import logging
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
 
-from rewards.clients.rewards_service_client import RewardsServiceClient
+from rewards.clients.rewards_service_client import RewardsServiceClient, AddedOrders, GetAllInfo
 
 #import model
-from .models import OrderData
+from models import OrderData
 
 class RewardsView(TemplateView):
     template_name = 'index.html'
@@ -32,7 +32,7 @@ class RewardsView(TemplateView):
 class AddInfo(TemplateView):
     template_name = 'index.html'
 
-    def __init__(self, logger=logging.getLogger(__name__)):
+    def __init__(self, logger=logging.getLogger(__name__), rewards_service_client=AddedOrders()):
         self.logger = logger
 
     def post(self, request, *args, **kwargs):
@@ -59,3 +59,22 @@ class AddInfo(TemplateView):
                 self.template_name,
                 context
             )
+
+class UserRewards(TemplateView):
+    template_name = 'index.html'
+
+    def __init__(self, logger=logging.getLogger(__name__), rewards_service_client=GetAllInfo()):
+        self.logger = logger
+        self.rewards_service_client = rewards_service_client
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        all_info = self.rewards_service_client.get_all_info()
+        context['all info'] = all_info
+
+        return TemplateResponse(
+            request,
+            self.template_name,
+            context
+        )
