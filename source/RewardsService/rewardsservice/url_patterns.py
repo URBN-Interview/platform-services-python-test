fddcbe478e853a448e4f615faf57e0b0c485af2a
altha22n <altha22n@mtholyoke.edu>
2020-08-17 21:36:22 -0400

    Adding customer_handler.py and email_address and order total parameters

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,7 +1,9 @@
 from handlers.rewards_handler import RewardsHandler
 from handlers.landing_page import LandingPage
+# from handlers.customer_handler import CustomerHandler
 
 url_patterns = [
 	(r'/', LandingPage),
     (r'/rewards', RewardsHandler),
+    # (r'/customers', CustomerHandler)
 ]
