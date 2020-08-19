f655f3165286607260daf0ba5efb8b71b4e0622c
Joie Murphy <joiermurphy@gmail.com>
2020-08-19 11:41:54 -0400

    Add django forms to handle form data for orders and email search
    Allow submission of order data.
    Allow search by email

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -6,6 +6,7 @@ class RewardsServiceClient:
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
         self.customers_url = "http://rewardsservice:7050/customers"
+        self.customer_url = "http://rewardsservice:7050/customerrewards"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
@@ -14,3 +15,10 @@ class RewardsServiceClient:
     def get_customers(self):
     	response = requests.get(self.customers_url)
     	return response.json()
+
+    def get_customer(self, email_address):
+    	response = requests.get(self.customer_url, {"emailAddress": email_address})
+    	return response.json()
+
+    def add_order_rewards(self, email_address, order_total):
+    	response = requests.post(url=self.customer_url, data={"emailAddress": email_address, "orderTotal": order_total})
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
+from django import forms
 
 from rewards.clients.rewards_service_client import RewardsServiceClient
 
@@ -19,11 +20,50 @@ class RewardsView(TemplateView):
         rewards_data = self.rewards_service_client.get_rewards()
         customers_data = self.rewards_service_client.get_customers()
 
-        context['rewards_data'] = rewards_data
-        context['customers_data'] = customers_data
+        if request.method == 'GET':
+            form = SearchForm(request.GET)
+            # replace list of all customers with the curstomer found from email search, might return nothing for customers
+            if form.is_valid():
+                customer_data = self.rewards_service_client.get_customer(form.cleaned_data['email_address'])
+                context['rewards_data'] = rewards_data
+                context['customers_data'] = customer_data
+            # form info was invalid or empty, use original tables
+            else:
+                context['rewards_data'] = rewards_data
+                context['customers_data'] = customers_data
+        else:
+            # original table info to load
+            context['rewards_data'] = rewards_data
+            context['customers_data'] = customers_data
 
         return TemplateResponse(
             request,
             self.template_name,
             context
         )
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        if request.method == 'POST':
+            form = OrderForm(request.POST)
+            # add or update customer order info
+            if form.is_valid():
+                self.rewards_service_client.add_order_rewards(form.cleaned_data['order_email_address'], form.cleaned_data['order_total'])
+        # original table info to load
+        rewards_data = self.rewards_service_client.get_rewards()
+        customers_data = self.rewards_service_client.get_customers()
+        context['rewards_data'] = rewards_data
+        context['customers_data'] = customers_data
+        return TemplateResponse(
+            request,
+            self.template_name,
+            context
+        )
+
+class SearchForm(forms.Form):
+    email_address = forms.CharField(label="Email Address", max_length=254)
+
+class OrderForm(forms.Form):
+    order_email_address = forms.CharField(label="Email Address", max_length=254)
+    order_total = forms.FloatField(label="Order Total")
