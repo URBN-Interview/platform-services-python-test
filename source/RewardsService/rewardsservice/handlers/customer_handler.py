b554ba3d76d8a59641e27a3b41fc58a2f94ebd88
altha22n <altha22n@mtholyoke.edu>
2020-08-18 23:22:21 -0400

    changing email_address variable to test@test.com which exists in the DB to make sure it is properly getting emails from the database

--- source/RewardsService/rewardsservice/handlers/customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_handler.py
@@ -12,20 +12,22 @@ class CustomerHandler(tornado.web.RequestHandler):
     @coroutine
     def get(self):
     	# self.write("testing")
-    	client = MongoClient("mongodb", 27017)
+        client = MongoClient("mongodb", 27017)
     	# self.write(client.list_database_names())
-    	db = client["Customers"]
-    	customers = list(db.customers.find({}, {"_id": 0}))
-    	self.write(json.dumps(customers))
-    	email_address = self.get_argument("emailAddress")
-        order_total = self.get_argument("orderTotal")
+        db = client["Customers"]
+        customers = list(db.customers.find({}, {"_id": 0}))
+        self.write(json.dumps(customers))
+        # email_address = "test@test.com"
+        email_address = "test@test.com"
+        # order_total = self.get_argument("orderTotal")
         # self.get_argument("emailAddress")
-    	email_exists = db.customers.find_one({'emailAddress': email_address}, {'_id': 0})
-    	#if customer does not already exist
-    	if not email_exists:
+        email_exists = db.customers.find_one({'emailAddress': email_address}, {'_id': 0})
+        # order_total = self.get_argument("orderTotal")
+        #if customer does not already exist
+        if not email_exists:
             rewardPoints = 0
             self.write(json.dumps(list(db.customers.find({}, {"_id": 0}))))
     		#insert a new one to the database
             db.customers.insert({'emailAddress': email_address, 'rewardPoints': rewardPoints})
-    	else: 
-    		self.write(json.dumps(db.customers.find_one({"emailAddress": email_address}, {"_id": 0})))
+        else: 
+          self.write(json.dumps(db.customers.find_one({"emailAddress": email_address}, {"_id": 0})))
