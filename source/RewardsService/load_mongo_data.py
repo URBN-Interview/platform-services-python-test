4d1e6a5f635642a58f0a53addb28d112fa7302ec
Joie Murphy <joiermurphy@gmail.com>
2020-08-19 00:05:29 -0400

    Change style of db fields
    Changed customers fields to camelCase

--- source/RewardsService/load_mongo_data.py
+++ source/RewardsService/load_mongo_data.py
@@ -23,8 +23,8 @@ def main():
     print("Removing and reloading customers in mongo")
     db = client["Customers"]
     db.customers.remove()
-    db.customers.insert({"email_address": "coolemail1@gmail.com", "reward_points": 650, "reward_tier": "F", "reward_tier_name": "30% off purchase", "next_reward_tier": "G", "next_reward_tier_name": "35% off purchase", "next_reward_tier_progress": 0.5})
-    db.customers.insert({"email_address": "myawesomeemail@outlook.com", "reward_points": 750, "reward_tier": "G", "reward_tier_name": "35% off purchase", "next_reward_tier": "H", "next_reward_tier_name": "40% off purchase", "next_reward_tier_progress": 0.5})
+    db.customers.insert({"emailAddress": "coolemail1@gmail.com", "rewardPoints": 650, "rewardTier": "F", "rewardTierName": "30% off purchase", "nextRewardTier": "G", "nextRewardTierName": "35% off purchase", "nextRewardTierProgress": 0.5})
+    db.customers.insert({"emailAddress": "myawesomeemail@outlook.com", "rewardPoints": 750, "rewardTier": "G", "rewardTierName": "35% off purchase", "nextRewardTier": "H", "nextRewardTierName": "40% off purchase", "nextRewardTierProgress": 0.5})
     print("Customers loaded in mongo")
 
 if __name__ == "__main__":
