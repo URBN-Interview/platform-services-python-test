b15d65a2bee66c230a495762e1c4b0241b16caa4
altha22n <altha22n@mtholyoke.edu>
2020-08-19 21:31:17 -0400

    Fixing some bugs with the logic of adding and updating rewards as well as move get_customer_reward into customer_handler.py to satisfy OOD principles

--- source/RewardsService/rewardsservice/handlers/customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_handler.py
@@ -3,19 +3,21 @@ import tornado.web
 
 from pymongo import MongoClient
 from tornado.gen import coroutine
+import math
 
 
 #following OOD design best practices, I am creating a class to handle the customer operations and rewards
 class CustomerHandler(tornado.web.RequestHandler):
-    client = MongoClient("mongodb", 27017)
 
     @coroutine
-    def get(self):
-        # self.write(client.list_database_names()) 
-        db = self.client["Customers"]
-        email_address = self.get_argument("emailAddress", None)
-        self.write(db.customers.find_one({"emailAddress": email_address}, {"_id": 0}))
-        # if not email_exists:
+    def get(self): 
+        ""
+        # client = MongoClient("mongodb", 27017)
+        # # self.write(client.list_database_names()) 
+        # db = client["Customers"]
+        # email_address = self.get_argument("emailAddress", None)
+        # self.write(db.customers.find_one({"emailAddress": email_address}, {"_id": 0}))
+        # # if not email_exists:
         #     self.write("This email doesn't exist in the system!")
         #     #insert a new one to the database
         # else: 
@@ -26,32 +28,68 @@ class CustomerHandler(tornado.web.RequestHandler):
     # gets the email addrress and order total
     @coroutine
     def post(self):
-    	# self.write("testing")
-    	# self.write(client.list_database_names())
-        # self.write(json.dumps(customers))
-        # email_address = "test@test.com"
-        db = self.client["Customers"]
+        client = MongoClient("mongodb", 27017)
+        db = client["Customers"]
         email_address = self.get_argument("emailAddress", None)
         order_total = self.get_argument("orderTotal", None)
-        # remove decimals
-        order_total_trun = trun(order_total)
-        # self.get_argument("emailAddress")
-        email_exists = db.customers.find_one({"emailAddress": email_address}, {"_id": 0})
-        # order_total = self.get_argument("orderTotal")
-        #customers earn 1 point for each dollar they spend
-        rewardPoints = order_total_trun
+        email_address = "moretest@testing.com"
+        order_total = 100
+
+        customer_exists = db.customers.find_one({"emailAddress": email_address}, {"_id": 0})
+        self.write(json.dumps(db.customers.find_one({"emailAddress": email_address}, {"_id": 0})))
+        reward_points = order_total
+
         #if customer does not already exist
-        if not email_exists:
-            # self.write(json.dumps(list(db.customers.find({}, {"_id": 0}))))
-    		#insert a new one to the database
-            db.customers.insert({"emailAddress": email_address, "rewardPoints": rewardPoints})
+        if not customer_exists:
+            #insert a new one to the database
+            self.write("Email doesn't exists")
+            customer_reward = self.get_customer_reward(reward_points)
+            customer_reward.update({'emailAddress': email_address})
+            db.customers.insert(customer_reward)
+            self.write(json.dumps(db.customers.find_one({"emailAddress": email_address}, {"_id": 0})))
         else:
-            db.customers.update_one({"emailAddress" : email_address},{ "$set": {"rewardPoints" : customers["rewardPoints"] + rewardPoints}}) 
+            #simply update 
+            reward_points = float(customer_exists["rewardPoints"] + reward_points)
+            customer_reward = self.get_customer_reward(reward_points)
+            customer_reward.update({'emailAddress': email_address})
+            db.customers.update_one({"emailAddress": email_address},
+                {'$set': customer_reward})
+            self.write("email exists")
             self.write(json.dumps(db.customers.find_one({"emailAddress": email_address}, {"_id": 0})))
 
-    # def get_rewards(self, email_address):
+        #function that returns the customer reward information
+    def get_customer_reward(self,reward_points):
+        #round down to whole number
+        rounded_reward_points = math.floor(reward_points)
+        #get current and next reward points - if the points are more than or equal to 1000 (tier F), there's no next reward
+        if rounded_reward_points >= 1000 : current_reward_points = 1000 
+        else : current_reward_points = rounded_reward_points
+        
+        if current_reward_points >= 1000 : next_reward_points = 1000
+        else : next_reward_points = current_reward_points + 100
 
-    # def set_rewards(self):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        current_reward = db.rewards.find_one(
+            {"points": current_reward_points},
+            {"_id": 0, "points": 0})
+        next_reward = db.rewards.find_one(
+            {"points": next_reward_points},
+            {"_id": 0, "points": 0})
+        # if current reward does not exist, it means this is a new customer starting off
+        if not current_reward:
+            current_reward = {"tier": "0", "rewardName": "0% off purchase"}
+        #this gets the decimal percent based on the example. E.g 1 - 400/500 = 0.2 
+        if current_reward_points == 1000: progress = 0
+        else : progress = 1 - (current_reward_points /next_reward_points)
+        #return all the fields info  
+        return({
+            "rewardPoints": reward_points,
+            "rewardTier": current_reward["tier"],
+            "rewardTierName": current_reward["rewardName"],
+            "nextRewardTier": next_reward["tier"],
+            "nextRewardTierName": next_reward["rewardName"],
+            "nextRewardTierName": progress})
 
 
 
