ff5f5801ed6ea66305be78306df91c0e89ebb857
Drew Zucker <drew.zucker@parivedasolutions.com>
2020-08-23 17:43:35 -0400

    handle top tier cases

--- source/RewardsService/rewardsservice/handlers/customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_handler.py
@@ -9,19 +9,9 @@ class SingleCustomerHandler(tornado.web.RequestHandler):
 
     @coroutine
     def get(self):
-        self.write('<html><body><form action="/customer-rewards" method="POST">'
-                   '<label for="email">Email Address:</label>'
-                   '<br>'
-                   '<input type="text" name="email address">'
-                   '<br>'
-                   '<input type="submit" value="Submit">'
-                   '</form></body></html>')
-    
-    @coroutine
-    def post(self):
         client = MongoClient("mongodb", 27017)
         db = client["Rewards"]
-        customer_email = self.get_body_argument("email address")
+        customer_email = self.get_argument("email_address")
         customer = db.orders.find_one({"emailAddress": customer_email})
         self.write(json.dumps(customer, default=str))
 
--- source/RewardsService/rewardsservice/handlers/orders_handler.py
+++ source/RewardsService/rewardsservice/handlers/orders_handler.py
@@ -7,6 +7,7 @@ from tornado.gen import coroutine
 
 from rewardsservice.order_calculator import OrderCalculator
 
+
 class OrdersHandler(tornado.web.RequestHandler):
 
     @coroutine
@@ -22,23 +23,30 @@ class OrdersHandler(tornado.web.RequestHandler):
                    '<br>'
                    '<input type="submit" value="Submit">'
                    '</form></body></html>')
+
     @coroutine
     def post(self):
         client = MongoClient("mongodb", 27017)
         db = client["Rewards"]
-        customer_record = OrderCalculator.get_email_record(self, self.get_body_argument("email address"), db)
+        customer_record = OrderCalculator.get_email_record(
+            self, self.get_body_argument("email address"), db)
         if customer_record is not None:
-            OrdersHandler.add_order_to_existing_customer(self, db, customer_record)
+            OrdersHandler.add_order_to_existing_customer(
+                self, db, customer_record)
         else:
             OrdersHandler.create_new_customer_record(self, db)
-        customer = db.orders.find_one({"emailAddress": self.get_body_argument("email address")})
+        customer = db.orders.find_one(
+            {"emailAddress": self.get_body_argument("email address")})
         self.write(json.dumps(customer, default=str))
 
     def create_new_customer_record(self, db):
-        reward_points = OrderCalculator.get_reward_points(self, self.get_body_argument("order total"))
+        reward_points = OrderCalculator.get_reward_points(
+            self, self.get_body_argument("order total"))
         tier_record = OrderCalculator.get_tier_record(self, reward_points)
-        next_tier_record = OrderCalculator.get_next_tier_record(self, tier_record["tier"])
-        percentage_to_next_tier = OrderCalculator.get_percentage_to_next_tier(self, reward_points, next_tier_record["points"])
+        next_tier_record = OrderCalculator.get_next_tier_record(
+            self, tier_record["tier"])
+        percentage_to_next_tier = OrderCalculator.get_percentage_to_next_tier(
+            self, reward_points, next_tier_record["points"])
         db.orders.insert({
             "emailAddress": self.get_body_argument("email address"),
             "rewardPoints": reward_points,
@@ -47,23 +55,26 @@ class OrdersHandler(tornado.web.RequestHandler):
             "nextRewardTier": next_tier_record["tier"],
             "nextRewardTierName": next_tier_record["rewardName"],
             "nextRewardTierProgress": percentage_to_next_tier,
-            })
+        })
 
     def add_order_to_existing_customer(self, db, customer_record):
-        reward_points = customer_record["rewardPoints"] + OrderCalculator.get_reward_points(self, self.get_body_argument("order total"))
+        reward_points = customer_record["rewardPoints"] + OrderCalculator.get_reward_points(
+            self, self.get_body_argument("order total"))
         tier_record = OrderCalculator.get_tier_record(self, reward_points)
-        next_tier_record = OrderCalculator.get_next_tier_record(self, tier_record["tier"])
-        percentage_to_next_tier = OrderCalculator.get_percentage_to_next_tier(self, reward_points, next_tier_record["points"])
+        next_tier_record = OrderCalculator.get_next_tier_record(
+            self, tier_record["tier"])
+        percentage_to_next_tier = OrderCalculator.get_percentage_to_next_tier(
+            self, reward_points, next_tier_record["points"])
         db.orders.update(
-            {"emailAddress":customer_record["emailAddress"]},
+            {"emailAddress": customer_record["emailAddress"]},
             {
-                "$set":{
-                    "rewardPoints":reward_points,
+                "$set": {
+                    "rewardPoints": reward_points,
                     "rewardTier": tier_record["tier"],
                     "rewardTierName": tier_record["rewardName"],
                     "nextRewardTier": next_tier_record["tier"],
                     "nextRewardTierName": next_tier_record["rewardName"],
                     "nextRewardTierProgress": percentage_to_next_tier,
-                    }
                 }
-            )
+            }
+        )
