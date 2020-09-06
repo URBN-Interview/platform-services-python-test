6b6b08378ad1a85509d6f01c0186a4fe716d7962
Sarah Melson <sarahmelson29@gmail.com>
2020-09-05 22:09:02 -0400

    copied and pasted the existing endpoint with a new URL to make sure I understood how it worked since I have never worked with Python, MongoDB, or Docker before. IT WORKED! Also added in a function to find the customer's reward tier.

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -14,4 +14,35 @@ class RewardsHandler(tornado.web.RequestHandler):
         rewards = list(db.rewards.find({}, {"_id": 0}))
         self.write(json.dumps(rewards))
 
-#This is a test to make sure I have git set up correctly! :)
+class CustomerHandler(tornado.web.RequestHandler):
+
+    @coroutine
+    def get(self):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        rewards = list(db.rewards.find({}, {"_id": 0}))
+        self.write(json.dumps(rewards))
+
+    def findRewardTier(self, totalSpent):
+        if totalSpent >= 1000:
+            return "J"
+        elif totalSpent >= 800 and totalSpent < 900:
+            return "I"
+        elif totalSpent >= 700 and totalSpent < 800:
+            return "H"
+        elif totalSpent >= 600 and totalSpent < 700:
+            return "G"
+        elif totalSpent >= 500 and totalSpent < 600:
+            return "F"
+        elif totalSpent >= 400 and totalSpent < 500:
+            return "E"
+        elif totalSpent >= 300 and totalSpent < 400:
+            return "D"
+        elif totalSpent >= 200 and totalSpent < 300:
+            return "C"
+        elif totalSpent >= 100 and totalSpent < 200:
+            return "B"
+        elif totalSpent > 0 and totalSpent < 100:
+            return "A"
+    
+
