4e81aed6e4ec879e8ec9cddf1d78c15ff621979e
Sarah Melson <sarahmelson29@gmail.com>
2020-09-06 23:46:10 -0400

    Worked a ton today on figuring out how to set up the first endpoint and store accurate data. I got a little frustrated so I decided to take a break from doing that and work on the other two endpoints. I was able to get them working! Yay! Also made some updates to my function that calculates the reward tier

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -14,35 +14,84 @@ class RewardsHandler(tornado.web.RequestHandler):
         rewards = list(db.rewards.find({}, {"_id": 0}))
         self.write(json.dumps(rewards))
 
+class OrderHandler(tornado.web.RequestHandler):
+
+    @coroutine
+    def post(self, keys):
+        #Connect to the database and make two new collections. One collection called Orders and the other called Customer
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        orderCollection = db["Orders"]
+        customerCollection = db["Customers"]
+
+        #Accept the parameters passed in by the user
+        email = self.get_argument("email", "default")
+        orderTotal = self.get_argument("orderTotal", "default")
+
+        #Add the order passed in by the user to the Users collection
+        newOrder = {"email": email, "orderTotal": orderTotal}
+        orderCollection.insert_one(newOrder)
+
+        #Pull a list of all the orders related to the email submitted and return to the sum of those orders for the purpose of finding the total dollar amount spent
+        query = {"email": email}
+        listOfOrders = list(orderCollection.find(query))
+        totalSpent = 0
+        for order in listOfOrders:
+            totalSpent += int(order.get("orderTotal"))
+
+        #Use the findRewardTiers method below to find the relevent reward tiers
+        rewardTiers = self.findRewardTiers(totalSpent)
+        returnedTiers = db.rewards.find({"$or": [{"tier": rewardTiers[0]}, {"tier": rewardTiers[1]}]}).sort("points", 1)
+
+        #Use the same query to determine if the customer's email already exists in the collection. 
+        customer = customerCollection.find_one(query)
+        if customer is None:
+            customer = {"email": email, "rewardPoints": int(totalSpent), "rewardTier": "", "rewardTierName": "", "nextRewardTier": "", "nextRewardTierName": "", "nextRewardTierProgress": ""}
+            customerCollection.insert_one(customer)
+        
+
+    def findRewardTiers(self, totalSpent):
+        if totalSpent >= 1000:
+            return list(["J","J"])
+        elif totalSpent >= 800 and totalSpent < 900:
+            return list(["H","I"])
+        elif totalSpent >= 700 and totalSpent < 800:
+            return list(["G","H"])
+        elif totalSpent >= 600 and totalSpent < 700:
+            return list(["F","G"])
+        elif totalSpent >= 500 and totalSpent < 600:
+            return list(["E","F"])
+        elif totalSpent >= 400 and totalSpent < 500:
+            return list(["D","E"])
+        elif totalSpent >= 300 and totalSpent < 400:
+            return list(["C","D"])
+        elif totalSpent >= 200 and totalSpent < 300:
+            return list(["B","C"])
+        elif totalSpent >= 100 and totalSpent < 200:
+            return list(["A","B"])
+        elif totalSpent < 100:
+            return list(["", "A"])
+
 class CustomerHandler(tornado.web.RequestHandler):
 
+    #Gets all customers
     @coroutine
     def get(self):
         client = MongoClient("mongodb", 27017)
         db = client["Rewards"]
-        rewards = list(db.rewards.find({}, {"_id": 0}))
-        self.write(json.dumps(rewards))
+        customerCollection = db["Customers"]
+        customers = list(db.rewards.customerCollection.find())
+        self.write(json.dumps(customers))
 
-    def findRewardTier(self, totalSpent):
-        if totalSpent >= 1000:
-            return "J"
-        elif totalSpent >= 800 and totalSpent < 900:
-            return "I"
-        elif totalSpent >= 700 and totalSpent < 800:
-            return "H"
-        elif totalSpent >= 600 and totalSpent < 700:
-            return "G"
-        elif totalSpent >= 500 and totalSpent < 600:
-            return "F"
-        elif totalSpent >= 400 and totalSpent < 500:
-            return "E"
-        elif totalSpent >= 300 and totalSpent < 400:
-            return "D"
-        elif totalSpent >= 200 and totalSpent < 300:
-            return "C"
-        elif totalSpent >= 100 and totalSpent < 200:
-            return "B"
-        elif totalSpent > 0 and totalSpent < 100:
-            return "A"
-    
+class CustomerDataHandler(tornado.web.RequestHandler):
+
+    #Accepts an email address and returns the rewards data
+    @coroutine
+    def get(self, keys):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        customerCollection = db["Customers"]
+        email = self.get_argument("email", "default")
+        query = {"email": email}
+        customer = list(customerCollection.find(query))
 
--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,8 @@
-from handlers.rewards_handler import RewardsHandler
+from handlers.rewards_handler import RewardsHandler, CustomerHandler, OrderHandler, CustomerDataHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/allCustomers', CustomerHandler),
+    (r'/order/(.*)', OrderHandler),
+    (r'/customer/(.*)', CustomerDataHandler)
 ]
