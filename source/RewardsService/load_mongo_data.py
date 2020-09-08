f9c4f7fe86125a2c2ea1ce1f678d800c832e8fe9
Siyi Guo <sguo@iuouosiyiguo.local>
2020-09-08 09:48:29 -0400

    define mongodb in app.py

--- source/RewardsService/load_mongo_data.py
+++ source/RewardsService/load_mongo_data.py
@@ -20,5 +20,41 @@ def main():
     db.rewards.insert({"points": 1000, "rewardName": "50% off purchase", "tier": "J"})
     print("Rewards loaded in mongo")
 
+    # Hard coded testing db	
+    db = client['Customers']	
+    print("Rendering all customers in mongo")	
+    db.customers.remove()	
+
+    db.customers.insert({	
+        "email" : "coco0@urbn.com",	
+        "orderTotal" : 100,	
+        "rewardName": "5% off purchase",	
+        "tier": "A",	
+        "points": 100,	
+        "nextReward": 0.10,	
+        "nextTier" : "B",	
+        "progress": 0.5	
+    })	
+    db.customers.insert({	
+        "email" : "coco1@urbn.com",	
+        "orderTotal" : 200,	
+        "rewardName": "10% off purchase",	
+        "tier": "B",	
+        "points": 200,	
+        "nextReward": 0.10,	
+        "nextTier" : "C",	
+        "progress" : 0.5	
+    })	
+    db.customers.insert({	
+        "email" : "coco2@urbn.com",	
+        "orderTotal" : 300,	
+        "rewardName": "15% off purchase",	
+        "tier": "C",	
+        "points": 300,	
+        "nextReward": 0.30,	
+        "nextTier" : "D",	
+        "progress": 0.5	
+    })
+
 if __name__ == "__main__":
     main()
