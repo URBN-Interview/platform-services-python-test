1b467c29f6628a949afcebf8c4aca887aaa65296
Maddie <madelinegeesen@mymail.mines.edu>
2020-09-30 20:24:11 -0600

    Finishing up
    Worked through the first part of RewardsUI, but cannot finish due to time constraints.

--- source/RewardsService/rewardsservice/handlers/all_customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/all_customer_handler.py
@@ -1,17 +1,15 @@
-import json
-import tornado.web
+import json	
+import tornado.web	
 
-from pymongo import MongoClient
-from tornado.gen import coroutine
+from pymongo import MongoClient	
+from tornado.gen import coroutine	
+from tornado.options import options	
+	
+class AllCustomerHandler(tornado.web.RequestHandler):	
 
-class AllCustomerHandler(tornado.web.RequestHandler):
-
-    @coroutine
-    def get(self):
-        client = MongoClient("mongodb", 27017)
-        db = client["Rewards"]
-        dbCustomer = db["Customers"]
-
-        customers = list(dbCustomer.find({}, {"_id" : 0}))
-        
-        self.write(json.dumps(customers))
+    @coroutine	
+    def get(self):	
+        client = MongoClient(options.mongodb_host)	
+        db = client["Customers"]	
+        customers = list(db.customers.find({}, {"_id": 0}))	
+        self.write(json.dumps(customers))	
--- source/RewardsService/rewardsservice/handlers/customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_handler.py
@@ -5,17 +5,16 @@ from pymongo import MongoClient
 from tornado.gen import coroutine	
 from tornado.options import options	
 
-class CustomerHandler(tornado.web.RequestHandler):	
-# GET Specific customer information	
+class CustomerHandler(tornado.web.RequestHandler):	=
     @coroutine	
     def get(self):	
         client = MongoClient(options.mongodb_host)	
-        db = client["Customers"]	
+        db = client["Customer"]	
 
         email = self.get_argument("email", "")	
 
         if email is not None:	
-            customers = list(db.customers.find({"email": email}, {"_id": 0}))	
+            customers = list(db.customers.find_one({"email": email}, {"_id": 0}))	
             self.write(json.dumps(customers))
         else:	
             print("email not found in database")	
