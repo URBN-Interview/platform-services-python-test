fc867a60ab0a9da3706ab3dfe293f586c51182fd
Maddie <madelinegeesen@mymail.mines.edu>
2020-09-30 18:07:42 -0600

    Revisions to orderHandler
    Made some changed to orderHandler to make code simpler and more readable

--- source/RewardsService/rewardsservice/handlers/order_handler.py
+++ source/RewardsService/rewardsservice/handlers/order_handler.py
@@ -10,80 +10,87 @@ class OrderHandler(tornado.web.RequestHandler):
 
     @coroutine	
     def post(self):	
-        client = MongoClient(options.mongodb_host)	
+        client = MongoClient("mongodb", 27017)	
         db = client["Customers"]	
-        rewardsDb = client["Rewards"]	
+        rewardsDb = client["Rewards"].rewards
 
         email = self.get_argument('email', '')	
         orderTot = self.get_argument('orderTotal', '')
-        points = orderTot
-
+        tier = self.tier(orderTot)
+        rewardName = self.rewardName(tier)
+        nextTier = self.nextTier(rewardName)
+        nextReward = self.nextReward(nextTier)
+        points = int(orderTot)
         existingCust = db.customers.find_one({"email": email}, {'_id':0})		
 
-        #if this customer already exists in the database, the total points should be their existing points + the points from this order
-        if(existingCust):	
-            points += existingCust['points']	
+        if(nextReward == ""):
+            progress = ""
+        else:
+            progress = orderTot/nextReward
 
-        cust = Customer(email, orderTot)	
-
-        #see if customer is eligible for reward
-        for r in rewardsDb.rewards.find({}, {"_id": 0}):	
-            if(points < r["points"]):	
-                nextReward = r
-                break	
-            curReward = r
-
-        if (curReward):	
-            cust.currentReward(
-                curReward["tier"], 
-                curReward['rewardName'], 
-                curReward['points']
-            )	
-	
-        if(nextReward):		
-            cust.newReward(
-                nextReward["tier"], 
-                nextReward["rewardName"], 
-                curReward["points"]/nextReward["points"]
-            )		
-	
         if(existingCust):	
             db.customers.update(
-                {'email': cust.email},	
+                {'email': email},	
                 {'points': points}
             )	
         else:	
             db.customers.insert(
-                {'email': cust.email, 
+                {'email': email, 
                 'points': points, 
-                'tier': cust.tier, 
-                'rewardName': cust.rewardName, 
-                'nextTier': cust.nextTier, 
-                'nextReward': cust.nextReward, 
-                'progress': cust.progress}
-            )	
+                'tier': tier, 
+                'rewardName': rewardName, 
+                'nextTier': nextTier, 
+                'nextReward': nextReward, 
+                'progress': progress}
+            )
+        
+    def tier(self, spent):
+        if (spent < 100):
+            return "N/A"
+        elif (spent < 200):
+            return "A"
+        elif (spent < 300):
+            return "B"
+        elif (spent < 400):
+            return "C"
+        elif (spent < 500):
+            return "D"
+        elif (spent < 600):
+            return "E"
+        elif (spent < 700):
+            return "F"
+        elif (spent < 800):
+            return "G"
+        elif (spent < 900):
+            return "H"
+        elif (spent < 1000):
+            return "I"
+        else:
+            return "J"
+        
+    def rewardName(self, rewardName):   
+        if (rewardName == "N/A"):
+            return "N/A"
+        for reward in rewardsDb:
+            if (reward["tier"] == rewardName):
+                return reward["rewardName"]
 
-        result = db.customers.find({"email": email}, {"_id": 0})
-        self.write(json.dumps(result))	
+    def nextTier(self, tier):
+        if (tier == ""):
+            return "A"
+        elif (tier == "J"):
+            return ""
+        else:
+            return chr(ord(tier) + 1)
+    
+    def nextReward(self, tier):
+        if (tier == ""):
+            return ""
+        for reward in rewardsDb:
+            if (reward["tier"] == tier):
+                return reward["points"]
+        return ""
 
-
-class Customer:	
-    def __init__(self, email, orderTotal):	
-        self.email = email	
-        self.orderTotal = orderTotal	
-        self.rewardName = ""	
-        self.tier = 0
-        self.points = 0.0
-        self.nextReward = 0.0	
-        self.nextTier = ""	
-        self.progress = 0.0	
-
-    def currentReward(self, tier, name, points):	
-        self.tier = tier	
-        self.rewardName = name	
-        self.points = points	
-
-    def newReward(self, tier, name, progress):	
-        self.tier = tier	
-        self.rewardName = name	
-        self.progress = progress
+client = MongoClient("mongodb", 27017)	
+db = client["Customers"]	
+rewardsDb = client["Rewards"].rewards
