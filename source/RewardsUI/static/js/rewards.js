bcc51a29fc3ff06a9f22ee359fc2b00859988b9a
zjswartz <33765589+zjswartz@users.noreply.github.com>
2020-10-15 15:22:01 -0400

    Implemented webapp cleanup

--- source/RewardsUI/static/js/rewards.js
+++ source/RewardsUI/static/js/rewards.js
@@ -1,23 +1,44 @@
 function buildCell(dataPoint){
   var cell = document.createElement('td'),
   textNode = document.createTextNode(dataPoint);
+  cell.classList.add('paddingTop10px');
   cell.appendChild(textNode);
   return cell;
+}
+function resetData(data){
+    getClientRewards('');
 }
 function submitForm(){
   var email = document.getElementById('email').value,
   amount = document.getElementById('amount').value,
   client = new XMLHttpRequest();
 
-  client.open("POST", "http://localhost:7050/orders", true);
-  client.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  client.send("email=" + email + "&amount=" + amount);
+  if(email === '' || amount === ''){
+      openSnackbar('snackbarRequiredInformation', 10000);
+  }else{
+      client.onreadystatechange = function() {
+        if (client.readyState === 4) {
+            document.getElementById('buttonAddRewards').style.display = 'inline-block';
+            document.getElementById('divAddRewardsIndicator').style.display = 'none';
+            if(client.response === ''){
+                openSnackbar('snackbarError', 4000);
+            }else{
+                openSnackbar('snackbarSuccess', 4000);
+                resetData(JSON.parse(client.response));
+            }
+        }
+      }
+      document.getElementById('buttonAddRewards').style.display = 'none';
+      document.getElementById('divAddRewardsIndicator').style.display = 'inline-block';
+      client.open("POST", "http://localhost:7050/orders", true);
+      client.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      client.send("email=" + email + "&amount=" + amount);
+  }
 }
 function displayData(data) {
   var tblClientRewards = document.getElementById('tblClientRewards'),
   tblBody = document.createElement('tbody');
   tblClientRewards.removeChild(tblClientRewards.getElementsByTagName('tbody')[0]);
-  //tblClientRewards.removeChild(tblClientRewards.getElementsByTagName('tbody'));
   tblClientRewards.appendChild(tblBody);
   for(var i=0;i<data.length;i++){
     var entry = data[i];
@@ -32,12 +53,17 @@ function displayData(data) {
     tblBody.appendChild(row);
   }
   tblClientRewards.appendChild(tblBody);
+  document.getElementById('tblClientRewards').style.display = 'table';
+  document.getElementById('divClientRewardsIndicator').style.display = 'none';
 
 }
 function getClientRewards(email){
   var client = new XMLHttpRequest(),
   data = null,
   param = (email === '') ? '' : '?email=' + email;
+
+  document.getElementById('tblClientRewards').style.display = 'none';
+  document.getElementById('divClientRewardsIndicator').style.display = 'block';
 
   client.onreadystatechange = function() {
     if (client.readyState === 4) {
@@ -48,6 +74,18 @@ function getClientRewards(email){
   client.open('GET', 'http://localhost:7050/orders' + param, true);
   client.send();
 }
-/* BEG: OnLoad function calls */
-getClientRewards('');
-/* END: OnLoad function calls */
+function adjustAddPanel(){
+    var divAddPanel = document.getElementById('divAddPanel'),
+    divAddButton = document.getElementById('divAddButton');
+    divAddButton.classList.toggle("active");
+    if (divAddPanel.style.maxHeight) {
+        divAddPanel.style.maxHeight = null;
+    } else {
+        divAddPanel.style.maxHeight = divAddPanel.scrollHeight + "px";
+    }
+}
+function openSnackbar(snackbarId, timeDelay){
+  var x = document.getElementById(snackbarId);
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, timeDelay);
+}
