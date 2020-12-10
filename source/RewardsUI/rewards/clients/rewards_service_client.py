3cabd51d70ef9d1e16f7ab637c3849a17729aec7
spence3645 <spence3645@gmail.com>
2020-12-09 17:15:11 -0800

    Add orders works and the display table shows all customers. Next, work on the search feature

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -9,3 +9,18 @@ class RewardsServiceClient:
     def get_rewards(self):
         response = requests.get(self.rewards_url)
         return response.json()
+
+class RewardMembersClient:
+    def __init__(self):
+        self.members_url = "http://rewardsservice:7050/rewardMembers"
+
+    def get_members(self):
+        response = requests.get(self.members_url)
+        return response.json()
+
+class OrderClient:
+    def __init__(self):
+        self.members_url = "http://rewardsservice:7050/postData"
+
+    def post_order(self, json):
+        r = requests.post(self.members_url, json=json)
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -1,17 +1,22 @@
 import logging
 
+from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
 
 from rewards.clients.rewards_service_client import RewardsServiceClient
+from rewards.clients.rewards_service_client import RewardMembersClient
+from rewards.clients.rewards_service_client import OrderClient
 
 
 class RewardsView(TemplateView):
     template_name = 'index.html'
 
-    def __init__(self, logger=logging.getLogger(__name__), rewards_service_client=RewardsServiceClient()):
+    def __init__(self, logger=logging.getLogger(__name__), rewards_service_client=RewardsServiceClient(), members_service_client=RewardMembersClient(), order_service_client=OrderClient()):
         self.logger = logger
         self.rewards_service_client = rewards_service_client
+        self.members_service_client = members_service_client
+        self.order_service_client = order_service_client
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
@@ -19,8 +24,43 @@ class RewardsView(TemplateView):
         rewards_data = self.rewards_service_client.get_rewards()
         context['rewards_data'] = rewards_data
 
+        members_data = self.members_service_client.get_members()
+        context['members_data'] = members_data
+
         return TemplateResponse(
             request,
             self.template_name,
             context
         )
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+
+        if request.POST.get("email"):
+            email = request.POST.get("email")
+            purchase = request.POST.get("purchase")
+
+            json = {"email": email, "purchase": purchase}
+
+            print(json)
+
+            self.order_service_client.post_order(json)
+
+            rewards_data = self.rewards_service_client.get_rewards()
+            context['rewards_data'] = rewards_data
+
+            members_data = self.members_service_client.get_members()
+            context['members_data'] = members_data
+
+        elif request.POST.get("search"):
+            email = request.POST.get("search")
+
+
+
+        return render(
+            request,
+            self.template_name,
+            context
+        )
+
+
