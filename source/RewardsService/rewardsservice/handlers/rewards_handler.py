8a78ec98bf1a05fdfd8f7ac5eff1b02fb5bfe5ee
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2020-12-16 20:50:56 -0500

    Adding the rewards endpoint back

---
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -0,0 +1,15 @@
+import json
+import tornado.web
+
+from pymongo import MongoClient
+from tornado.gen import coroutine
+
+
+class RewardsHandler(tornado.web.RequestHandler):
+
+    @coroutine
+    def get(self):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        rewards = list(db.rewards.find({}, {"_id": 0}))
+        self.write(json.dumps(rewards))
--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,8 +1,11 @@
 from handlers.order_handler import OrderHandler
 from handlers.customer_handler import CustomerHandler
 from handlers.customers_handler import CustomersHandler
+from handlers.rewards_handler import RewardsHandler
+
 
 url_patterns = [
+    (r'/rewards', RewardsHandler),
     (r'/order', OrderHandler),
     (r'/customer', CustomerHandler),
     (r'/customers', CustomersHandler),
