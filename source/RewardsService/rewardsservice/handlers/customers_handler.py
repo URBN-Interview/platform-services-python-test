4fdb7a1c71d99f5c9e09612c43159a3756936536
Logan May <loma@sas.upenn.edu>
2020-12-16 22:15:20 -0500

    add input validation logic and unit tests, echo back the data when it passes validation

--- source/RewardsService/rewardsservice/handlers/customers_handler.py
+++ source/RewardsService/rewardsservice/handlers/customers_handler.py
@@ -10,12 +10,64 @@ class CustomersHandler(tornado.web.RequestHandler):
     def post(self):
         try:
             request_body = json.loads(self.request.body.decode())
-            self.write(request_body)
         except UnicodeError:
             self.set_status(400)
             self.write("Request body must be utf-8 encoded")
-            self.finish()
+            return
         except json.decoder.JSONDecodeError:
             self.set_status(400)
             self.write("Invalid JSON")
-            self.finish()
+            return
+
+        email_address, error = self.validate_email_address(request_body)
+        if error is not None:
+            self.set_status(error.get("status_code"))
+            self.write(error.get("message"))
+            return
+
+        order_total, error = self.validate_order_total(request_body)
+        if error is not None:
+            self.set_status(error.get("status_code"))
+            self.write(error.get("message"))
+            return
+
+        self.write(f"email_address: {email_address}, order_total: {'{:.2f}'.format(order_total)}")
+
+
+    def validate_email_address(self, request_body):
+        if "email_address" not in request_body:
+            return None, {
+                "message": self.missing_field_error_message("email_address"),
+                "status_code": 400
+            }
+
+        # TODO: add validation here if not performed on client side
+        return request_body["email_address"], None
+
+
+    def validate_order_total(self, request_body):
+        if "order_total" not in request_body:
+            return None, {
+                "message": self.missing_field_error_message("order_total"),
+                "status_code": 400
+            }
+
+        try:
+            order_total = float(request_body["order_total"])
+        except ValueError:
+            return None, {
+                "message": "'order_total' must be a number",
+                "status_code": 400
+            }
+
+        if round(order_total, 2) != order_total:
+            return None, {
+                "message": "'order_total' must be a number with no more than two values after the decimal point",
+                "status_code": 400
+            }
+
+        return order_total, None
+
+
+    def missing_field_error_message(self, missing_field):
+        return f"Missing required field '{missing_field}'"
--- source/RewardsService/rewardsservice/test.py
+++ source/RewardsService/rewardsservice/test.py
@@ -33,7 +33,7 @@ class PostCustomersTests(AsyncHTTPTestCase):
         self.assertEqual(response.body, bytes("Invalid JSON","utf-8"))
 
     @gen_test
-    def test_valid_json(self):
+    def test_missing_email_address(self):
         client = AsyncHTTPClient()
         response = yield client.fetch(
             self.get_url("/customers"),
@@ -41,5 +41,66 @@ class PostCustomersTests(AsyncHTTPTestCase):
             method="POST",
             body='{"hello":"world"}'
             )
+        self.assertEqual(response.code, 400)
+        self.assertEqual(response.body, bytes("Missing required field 'email_address'", "utf-8"))
+
+    @gen_test
+    def test_have_email_address_missing_order_total(self):
+        client = AsyncHTTPClient()
+        response = yield client.fetch(
+            self.get_url("/customers"),
+            raise_error=False,
+            method="POST",
+            body='{"email_address":"loma@sas.upenn.edu"}'
+            )
+        self.assertEqual(response.code, 400)
+        self.assertEqual(response.body, bytes("Missing required field 'order_total'", "utf-8"))
+
+    @gen_test
+    def test_have_email_address_order_total_is_not_number(self):
+        client = AsyncHTTPClient()
+        response = yield client.fetch(
+            self.get_url("/customers"),
+            raise_error=False,
+            method="POST",
+            body='{"email_address":"loma@sas.upenn.edu", "order_total": "hello"}'
+            )
+        self.assertEqual(response.code, 400)
+        self.assertEqual(response.body, bytes("'order_total' must be a number", "utf-8"))
+
+    @gen_test
+    def test_have_email_address_order_total_needs_rounding(self):
+        client = AsyncHTTPClient()
+        response = yield client.fetch(
+            self.get_url("/customers"),
+            raise_error=False,
+            method="POST",
+            body='{"email_address":"loma@sas.upenn.edu", "order_total": 100.0001}'
+            )
+        self.assertEqual(response.code, 400)
+        self.assertEqual(response.body, bytes("'order_total' must be a number with no more than two values after the decimal point", "utf-8"))
+
+    @gen_test
+    def test_success_have_all_fields_order_total_is_int(self):
+        client = AsyncHTTPClient()
+        response = yield client.fetch(
+            self.get_url("/customers"),
+            raise_error=False,
+            method="POST",
+            body='{"email_address":"loma@sas.upenn.edu", "order_total": 100}'
+            )
         self.assertEqual(response.code, 200)
-        self.assertEqual(response.body, bytes('{"hello": "world"}', "utf-8"))
+        self.assertEqual(response.body, bytes("email_address: loma@sas.upenn.edu, order_total: 100.00", "utf-8"))
+
+    @gen_test
+    def test_success_have_all_fields_order_total_is_float(self):
+        client = AsyncHTTPClient()
+        response = yield client.fetch(
+            self.get_url("/customers"),
+            raise_error=False,
+            method="POST",
+            body='{"email_address":"loma@sas.upenn.edu", "order_total": 100.00}'
+            )
+        self.assertEqual(response.code, 200)
+        self.assertEqual(response.body, bytes("email_address: loma@sas.upenn.edu, order_total: 100.00", "utf-8"))
+
