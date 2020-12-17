00350626ce4f501367f269b954fe76d032139bac
Logan May <loma@sas.upenn.edu>
2020-12-17 18:01:50 -0500

    Endpoint1 creates and updates records correctly

--- source/RewardsService/rewardsservice/db.py
+++ source/RewardsService/rewardsservice/db.py
@@ -8,35 +8,60 @@ class Database():
         self.client = MongoClient("mongodb", 27017)
 
     def upsert_customer(self, email_address, order_total):
+        points_from_order = int(order_total) # rounds down
+
+        customer = self.get_customer_by_email_address(email_address)
+        if customer is None:
+            return self.insert_customer(email_address, points_from_order)
+
+        return self.update_customer(customer, points_from_order)
+
+
+    def insert_customer(self, email_address, points):
         db = self.client["Customers"]
+        record = self.new_customer_record(email_address, points)
+        db.customers.insert(record)
+        del record['_id'] # don't return Mongo internal IDs
+        return json.loads(json_util.dumps(record))
 
-        customer_exists = db.customers.count_documents({'email_address': email_address}) > 0
-        if not customer_exists:
-            record = self.new_customer_record(email_address, order_total)
-            db.customers.insert(record)
 
-        customer = list(db.customers.find({'email_address': email_address}))[0]
-        return json.loads(json_util.dumps(customer))
+    def update_customer(self, customer, points_from_order):
+        db = self.client["Customers"]
+        updated_customer = self.updated_customer_record(customer, points_from_order)
+        db.customers.find_one_and_replace(
+            {"email_address": updated_customer.get("email_address")},
+            updated_customer)
+        return json.loads(json_util.dumps(updated_customer))
 
-    def new_customer_record(self, email_address, order_total):
+
+    def new_customer_record(self, email_address, points):
         return {
             "email_address": email_address,
-            **self.new_reward_data_for_order_total(order_total)
+            **self.new_reward_data_for_points(points)
         }
 
-    def new_reward_data_for_order_total(self, order_total):
-        points = int(order_total) # rounds down
+
+    def updated_customer_record(self, customer, points_from_order):
+        total_points = customer.get("reward_points") + points_from_order
+        return {
+            "email_address": customer.get("email_address"),
+            **self.new_reward_data_for_points(total_points)
+        }
+
+
+    def new_reward_data_for_points(self, points):
         current_tier = self.get_current_tier_by_points(points)
         next_tier = self.get_next_tier_by_points(points)
 
         return {
-            "reward_points": current_tier.get("points", ""),
+            "reward_points": points,
             "reward_tier": current_tier.get("tier", ""),
             "reward_tier_name": current_tier.get("rewardName", ""),
             "next_reward_tier": next_tier.get("points", ""),
             "next_reward_tier_name": next_tier.get("tier", ""),
             "next_reward_tier_progress": next_tier.get("rewardName", "")
         }
+
 
     def get_current_tier_by_points(self, points):
         db = self.client["Rewards"]
@@ -45,9 +70,18 @@ class Database():
             return {}
         return json.loads(json_util.dumps(tier_list[0]))
 
+
     def get_next_tier_by_points(self, points):
         db = self.client["Rewards"]
         tier_list = list(db.rewards.find({"points": {"$gt": points}}).sort('points', ASCENDING))
         if len(tier_list) == 0:
             return {}
         return json.loads(json_util.dumps(tier_list[0]))
+
+
+    def get_customer_by_email_address(self, email_address):
+        db = self.client["Customers"]
+        customers_list = list(db.customers.find({'email_address': email_address}))
+        if len(customers_list) == 0:
+            return None
+        return json.loads(json_util.dumps(customers_list[0]))
