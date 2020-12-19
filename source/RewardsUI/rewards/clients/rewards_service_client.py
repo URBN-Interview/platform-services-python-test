e8ff78dd53717694358ac252db29a85e68d756ac
Zak Barbezat <zbarbezat@gmail.com>
2020-12-19 14:25:44 -0500

    Add implementation for printing all user data in table on start

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -6,6 +6,7 @@ class RewardsServiceClient:
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
         self.submit_url = "http://rewardsservice:7050/endpoint1"
+        self.allusers_url = "http://rewardsservice:7050/endpoint3"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
@@ -14,3 +15,8 @@ class RewardsServiceClient:
     def submit_order(self, email, amount):
         response = requests.get(self.submit_url, {'email': email, 'amount':amount})
         return response
+
+    def get_allusers(self):
+        response = requests.get(self.allusers_url)
+        return response.json()
+
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -10,6 +10,7 @@ from rewards.clients.rewards_service_client import RewardsServiceClient
 from django.views.decorators.csrf import csrf_exempt
 import socket
 import urllib
+import json
 
 class RewardsView(TemplateView):
     template_name = 'index.html'
@@ -24,6 +25,18 @@ class RewardsView(TemplateView):
         rewards_data = self.rewards_service_client.get_rewards()
         context['rewards_data'] = rewards_data
 
+        users_data = self.rewards_service_client.get_allusers()
+
+        for user in users_data:
+            val = user['nextProgress']
+            if val == 'N/A':
+                # don't need % mark on N/A string
+                pass
+            else:
+                user['nextProgress'] = str(round(user['nextProgress'] * 100, 1)) + '%'
+
+        context['users_data'] = users_data
+
         return TemplateResponse(
             request,
             self.template_name,
@@ -31,16 +44,17 @@ class RewardsView(TemplateView):
         )
     
     def post(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        email = request.POST.get('email')
-        amount = request.POST.get('amount')
+        if 'amount' in request.POST: # amount key only in submit form
+            context = self.get_context_data(**kwargs)
+            email = request.POST.get('email')
+            amount = request.POST.get('amount')
 
-        submit_data = self.rewards_service_client.submit_order(email, amount)
-        print(submit_data)
+            submit_data = self.rewards_service_client.submit_order(email, amount)
+            print(submit_data)
 
-        context['order_message'] = "Order of amount " + amount + " by " + email + " successfully sent!"
+            context['order_message'] = "Order of amount " + amount + " by " + email + " successfully sent!"
 
-        rewards_data = self.rewards_service_client.get_rewards()
-        context['rewards_data'] = rewards_data
+            rewards_data = self.rewards_service_client.get_rewards()
+            context['rewards_data'] = rewards_data
 
-        return render(request, self.template_name, context)
+            return render(request, self.template_name, context)
