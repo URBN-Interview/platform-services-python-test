43eb2399061aac23147e5fd3133db26924496a70
Zak Barbezat <zbarbezat@gmail.com>
2020-12-19 11:37:19 -0500

    Add implementation for front end order submit button

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -5,7 +5,12 @@ class RewardsServiceClient:
 
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
+        self.submit_url = "http://rewardsservice:7050/endpoint1"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
         return response.json()
+
+    def submit_order(self, email, amount):
+        response = requests.get(self.submit_url, {'email': email, 'amount':amount})
+        return response
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -1,10 +1,15 @@
 import logging
+import requests
+import time
 
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
+from django.shortcuts import render
 
 from rewards.clients.rewards_service_client import RewardsServiceClient
-
+from django.views.decorators.csrf import csrf_exempt
+import socket
+import urllib
 
 class RewardsView(TemplateView):
     template_name = 'index.html'
@@ -24,3 +29,18 @@ class RewardsView(TemplateView):
             self.template_name,
             context
         )
+    
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        email = request.POST.get('email')
+        amount = request.POST.get('amount')
+
+        submit_data = self.rewards_service_client.submit_order(email, amount)
+        print(submit_data)
+
+        context['order_message'] = "Order of amount " + amount + " by " + email + " successfully sent!"
+
+        rewards_data = self.rewards_service_client.get_rewards()
+        context['rewards_data'] = rewards_data
+
+        return render(request, self.template_name, context)
