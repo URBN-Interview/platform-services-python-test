fc5a26edc7769dd6863f1410a9df8714117aaf27
SneakoXU <51842434+SneakoXU@users.noreply.github.com>
2020-12-30 13:31:32 -0500

    Endpoints Implemented
    Endpoints implemented but not completely tested. Endpoint 1 only works for new customers, need to add logic for existing customers.

--- source/RewardsService/rewardsservice/handlers/customer_order_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_order_handler.py
@@ -9,8 +9,9 @@ class CustomerOrderHandler(tornado.web.RequestHandler):
     #accepts order data - email address and order total - create new table/collection? 
     def post(self):
         client = MongoClient("mongodb", 27017)
-        customers = client["Customers"]
+        
         db = client["Rewards"]
+        customers = db["Customers"]
         rewards = list(db.rewards.find({}, {"_id": 0}))
         customerReward = None
         nextTier = None
@@ -18,9 +19,8 @@ class CustomerOrderHandler(tornado.web.RequestHandler):
 
     #post email and order total
         #set email and orderTotal to user inputs
-        email = self.get_argument('email')
-        orderTotal = self.get_argument('orderTotal')
-
+        email = self.get_argument("email")
+        orderTotal = self.get_argument("orderTotal")
         #calc the rewardspoints based off orderTotal - aka round the total to the nearest int
         rewardsPoints = int(orderTotal)
 
@@ -29,20 +29,61 @@ class CustomerOrderHandler(tornado.web.RequestHandler):
         if(rewardsPoints < 1000):
             for rewardTier in rewards:
                 if(rewardsPoints>=rewardTier["points"]):
-                    customerReward = rewardTier     
+                    customerReward = rewardTier
+                elif(rewardsPoints < rewardTier["points"]):
+                    nextTier = rewardTier   
+                    break
     #if user has less than 100 points then they do not qualify for rewards    
             if(rewardsPoints < 100):
-                customerReward = {'rewardName' : 'No reward available', 'tier' : 'Not Enough Rewards Points Accumulated'}
-        
+                customerReward = {"rewardName" : "No reward available", "tier" : "Not Enough Rewards Points Accumulated"}
+            nextTierProgress = 100 * ( (customerReward["points"] - nextTier ["points"]) / 100 ) + "%"     
+       
+    
         #creating customerOrder to store data
-        customerOrder = {
-            'email': email, 
-            'points': rewardsPoints, 
-            'rewardName': customerReward['rewardName'], 
-            'tier': customerReward['tier']
+            customerOrder = {
+                "email": email, 
+                "points": rewardsPoints, 
+                "rewardName": customerReward["rewardName"], 
+                "tier": customerReward["tier"],
+                "nextTier" : nextTier["tier"],
+                "nextRewardName" : nextTier["rewardName"],
+                "progressToNextTier" : nextTierProgress
+
             }
         
         #adding customerOrder data to customers collection
-        customers.insert_one(customerOrder)
+            customers.insert_one(customerOrder)
         
-    
+        #set variables if highest tier is reached
+        else:
+            customerReward = {"rewardName" : rewardTier["rewardName"], "tier" : rewardTier["tier"]}   
+            customerOrder = {
+                "email": email, 
+                "points": rewardsPoints, 
+                "rewardName": customerReward["rewardName"], 
+                "tier": customerReward["tier"],
+                "nextTier" : "Reached highest tier.",
+                "nextRewardName" : "Reached highest tier.",
+                "progressToNextTier" : "NA"
+            }
+
+            customers.insert_one(customerOrder)
+
+class CustomerHandler(tornado.web.RequestHandler):
+    @coroutine
+    def get(self):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        customers = db["Customers"]
+        email = self.get_argument("email")
+        customer = list(customers.find_one({"email": email}))
+        self.write(json.dumps(customer))
+      
+class AllCustomersHandler(tornado.web.RequestHandler):
+    @coroutine
+    def get(self):
+        client = MongoClient("mongodb", 27017)
+        db = client["Rewards"]
+        customers = db["Customers"]
+        allCustomers = list(customers.find({}))
+        self.write(json.dumps(allCustomers))
--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,7 +1,12 @@
 from handlers.rewards_handler import RewardsHandler
+from handlers.customer_order_handler import CustomerOrderHandler, AllCustomersHandler, CustomerHandler
+
 
 
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/order', CustomerOrderHandler),
+    (r'/customer', CustomerHandler),
+    (r'/customers', AllCustomersHandler)
 ]
