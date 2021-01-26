64df2332cef5e4791975698874a229e4c8f53087
swirawan <stanleywirawan@gmail.com>
2021-01-26 00:50:31 -0500

    add more tests

--- source/RewardsService/unit_test.py
+++ source/RewardsService/unit_test.py
@@ -10,6 +10,8 @@ class RewardServiceTest(unittest.TestCase):
     def setUpClass(cls):
         cls.base_url = "http://localhost:7050"
         cls.rewards_url = cls.base_url + "/rewards"
+        cls.customer_url = cls.base_url + "/me/rewards"
+        cls.list_rewards_url = cls.base_url + "/rewards/customers"
 
         client = MongoClient("localhost", 27017)
         cls.db = client["Rewards"]
@@ -51,6 +53,26 @@ class RewardServiceTest(unittest.TestCase):
     def test_post_rewards_without_order_total_params(self):
         response = requests.post(self.rewards_url, params={"email_address": self.email_address})
         self.assertEqual(500, response.status_code)
+
+    def test_get_customers(self):
+        requests.post(self.rewards_url,
+                                 params={"email_address": self.email_address, "order_total": self.order_total})
+        response = requests.get(self.customer_url, params={"email_address":self.email_address})
+        self.assertEqual(200, response.status_code)
+
+        response_body = ast.literal_eval(response.content.decode("utf-8"))
+        self.assertEqual(self.email_address, response_body.get("emailAddress"))
+
+        for key in ["_id", "rewardTierName", "emailAddress", "tier", "points", "nextTier", "progress"]:
+            self.assertIn(key, response_body)
+
+    def test_get_customers_without_email_address(self):
+        response = requests.get(self.customer_url)
+        self.assertEqual(400, response.status_code)
+
+    def test_get_list_of_rewards(self):
+        response = requests.get(self.list_rewards_url)
+        self.assertEqual(200, response.status_code)
 
 
 if __name__ == "__main__":
