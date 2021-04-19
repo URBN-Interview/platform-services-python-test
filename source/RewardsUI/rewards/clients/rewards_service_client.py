10687a93d267b21e91a92479116edaebed4f82b2
christianlomboy <christian.lomboy@icloud.com>
2021-04-19 15:24:17 -0400

    Define add_order function in client & add /order endpoint

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -6,6 +6,7 @@ class RewardsServiceClient:
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
         self.customers_url = "http://rewardsservice:7050/customers"
+        self.order_url = "http://rewardsservice:7050/order"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
@@ -14,3 +15,7 @@ class RewardsServiceClient:
     def get_customers(self):
         response = requests.get(self.customers_url)
         return response.json()
+
+    def add_order(self, email_address, order_total):
+        response = requests.post(self.order_url, {"emailAddress": email_address, "orderTotal": order_total})
+        return response
