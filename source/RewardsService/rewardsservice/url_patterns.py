64590f370edae23f1739b57ac1b537d363a25093
ajaipuri <ajaipuri@cccis.com>
2021-10-14 23:03:48 -0400

    Updated the first endpoint url

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,7 @@
 from handlers.rewards_handler import RewardsHandler
+from handlers.customer_order_rewards_handler import CustomerOrderRewardsHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/customer_order_rewards', CustomerOrderRewardsHandler)
 ]
