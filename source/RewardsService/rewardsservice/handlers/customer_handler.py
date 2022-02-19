60f131bf287fc136a93f9ac77d23657d10aa20c6
risingsunomi <vincentcastro@gmail.com>
2022-02-19 05:07:00 -0900

    cleaning up api code and adding more error handling

--- source/RewardsService/rewardsservice/handlers/customer_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_handler.py
@@ -21,31 +21,43 @@ class CustomerHandler(tornado.web.RequestHandler):
     # return all customer
     @coroutine
     def get(self):
-        client = MongoClient("mongodb", 27017)
-       
-        # get customers in customer collection from Rewards db
-        db = client["Rewards"]
-        customers = list(db.customers.find({}, {"_id": 0}))
-        json_response = {"customers": customers}
+        json_response = {}
 
+        try:
+            client = MongoClient("mongodb", 27017)
+        
+            # get customers in customer collection from Rewards db
+            db = client["Rewards"]
+            customers = list(db.customers.find({}, {"_id": 0}))
+            json_response = {"customers": customers}
+        except Exception as err:
+            raise err
+        
         self.write(json.dumps(json_response))
     
     # return customer information from email
     @coroutine
     def post(self):
-        client = MongoClient("mongodb", 27017)
-        db = client["Rewards"]
-        
-        # get post data
-        json_request = tornado.escape.json_decode(self.request.body)
+        json_response = {}
 
-        if json_request["email"] == "":
-            customer = list(db.customers.find({}, {"_id": 0}))
-        else:
-            customer = list(db.customers.find(
-                {"email": json_request["email"]},
-                {"_id": 0}
-            ))
+        try:
+            client = MongoClient("mongodb", 27017)
+            db = client["Rewards"]
+            
+            # get post data
+            json_request = tornado.escape.json_decode(self.request.body)
 
-        self.write(json.dumps({"customer": customer}))
+            if json_request["email"] == "":
+                customer = list(db.customers.find({}, {"_id": 0}))
+            else:
+                customer = list(db.customers.find(
+                    {"email": json_request["email"]},
+                    {"_id": 0}
+                ))
+
+            json_response = {"customer": customer}
+        except Exception as err:
+            raise err
+
+        self.write(json.dumps(json_response))
         
--- source/RewardsService/rewardsservice/tests/test_customer_handler.py
+++ source/RewardsService/rewardsservice/tests/test_customer_handler.py
@@ -6,6 +6,7 @@ import json
 import requests
 
 class TestCustomerHandler(unittest.TestCase):
+    # test get of list of customers and see if returns 200 status
     def test_get_customer(self):
         response = requests.get(
             "http://localhost:7050/customers", 
@@ -15,10 +16,9 @@ class TestCustomerHandler(unittest.TestCase):
             }
         )
 
-        print(response.json())
-
         self.assertEqual(200, response.status_code)
 
+    # test post where it is being used to search for customer by email
     def test_post_customer(self):
         query = {"email": "test@test.com"}
         
@@ -32,8 +32,6 @@ class TestCustomerHandler(unittest.TestCase):
                 "Accept": "text/plain"
             }
         )
-
-        print(response)
 
         self.assertEqual(200, response.status_code)
 
