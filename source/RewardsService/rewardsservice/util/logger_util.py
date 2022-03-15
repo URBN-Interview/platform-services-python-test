a149d0b5d03fab9f84f7abcd1730a94c476ba8cf
rajeshKolluri <rajkolluri@gmail.com>
2022-03-15 13:58:25 -0400

    Adding the logging util

---
+++ source/RewardsService/rewardsservice/util/logger_util.py
@@ -0,0 +1,15 @@
+import logging
+
+class loggingUtil:
+    def get_module_logger(mod_name):
+        """
+        To use this, do logger = get_module_logger(__name__)
+        """
+        logger = logging.getLogger(mod_name)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+        return logger
