cbe75dab25c466e2d382a2612a34815aed2395cb
darwinhang <darwin.hang@gmail.com>
2022-05-01 23:41:04 -0500

    add: validation on POST

--- source/RewardsService/rewardsservice/handlers/customer_rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_rewards_handler.py
@@ -25,9 +25,6 @@ class SingleCustomerHandler(tornado.web.RequestHandler):
 
 
 class CustomerRewardsHandler(tornado.web.RequestHandler):
-
-    # TODO("Implement. if email address provided, then return single, otherwise return all")
-    # won't paginate due to time, but in real life should
     @ coroutine
     def get(self):
         client = MongoClient("mongodb", 27017)
@@ -35,30 +32,22 @@ class CustomerRewardsHandler(tornado.web.RequestHandler):
         rewards = list(db.customer_rewards.find({}, {"_id": 0}))
         self.write(json.dumps(rewards))
 
-    # * **Endpoint 1:**
-    #     * Accept a customer's order data: **email adress**  (ex. "customer01@gmail.com") and **order total** (ex. 100.80).
-    #     * Calculate and store the following customer rewards data into MongoDB. For each dollar a customer spends, the customer will earn 1 reward point. For example, an order of $100.80 earns 100 points. Once a customer has reached the top rewards tier, there are no more rewards the customer can earn.
-    #         * **Email Address:** the customer's email address (ex. "customer01@gmail.com")
-    #         * **Reward Points:** the customer's rewards points (ex. 100)
-    #         * **Reward Tier:** the rewards tier the customer has reached (ex. "A")
-    #         * **Reward Tier Name:** the name of the rewards tier (ex. "5% off purchase")
-    #         * **Next Reward Tier:** the next rewards tier the customer can reach (ex. "B")
-    #         * **Next Reward Tier Name:** the name of next rewards tier (ex. "10% off purchase")
-    #         * **Next Reward Tier Progress:** the percentage the customer is away from reaching the next rewards tier (ex. 0.5)
-    # {
-    #     "email_address": "string",
-    #     "rewards_points": "number",
-    #     "reward_tier": "string",
-    #     "reward_tier_name": "string",
-    #     "next_reward_tier": "string",
-    #     "next_reward_tier_name": "string",
-    #     "next_reward_tier_progress": "number"
-    # }
     @ coroutine
     def post(self):
         body = tornado.escape.json_decode(self.request.body)
-        email_address = body["emailAddress"]
-        # check for email and order_total
+        try:
+            email_address = body["emailAddress"]
+            order_total = float(body["orderTotal"])
+        except KeyError:
+            self.set_status(400)
+            self.write(json.dumps(
+                {"message": "Invalid Request. 'emailAddress' and 'orderTotal' required"}))
+            return
+        except ValueError:
+            self.set_status(400)
+            self.write(json.dumps(
+                {"message": "Invalid Request. 'emailAddress' must be a string and 'orderTotal' must be a number"}))
+            return
         # Initialize in `initialize` block
         client = MongoClient("mongodb", 27017)
         db = client["Rewards"]
