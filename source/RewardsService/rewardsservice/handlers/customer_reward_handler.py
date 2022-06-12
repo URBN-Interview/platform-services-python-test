7410d5b40a91b8e8335a7dee4779289790321325
parth <parth.patel.r@gmail.com>
2022-06-12 17:14:04 -0400

    data guard and Validation

--- source/RewardsService/rewardsservice/handlers/customer_reward_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_reward_handler.py
@@ -1,13 +1,14 @@
 import json
 import tornado.web
 from tornado.gen import coroutine
+import re
 
 
 class CustomerRewardsHandler(tornado.web.RequestHandler):
 
     @coroutine
     def get(self):
-        email = self.get_email()
+        email = self.get_email(False)
         if email:
             customers = self.get_customer_by_email(email)
         else:
@@ -15,8 +16,13 @@ class CustomerRewardsHandler(tornado.web.RequestHandler):
         self.set_header('Content-Type', 'application/json')
         self.write(json.dumps(customers))
 
-    def get_email(self):
+    def get_email(self, req):
         email = self.get_argument('email', None, True)
+        if req and not email:
+            raise ValueError('Email is required')
+        regex = r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}'
+        if(req and not re.fullmatch(regex, email)):
+            raise ValueError(email + ' is not a valid email')
         return email
 
     def get_customer_by_email(self, email):
