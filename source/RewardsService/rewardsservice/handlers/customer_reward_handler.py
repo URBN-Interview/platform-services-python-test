b1a2a5df979387e445b0118e65d73786f0399c18
parth <parth.patel.r@gmail.com>
2022-06-12 17:36:28 -0400

    Create Customer POST request

--- source/RewardsService/rewardsservice/handlers/customer_reward_handler.py
+++ source/RewardsService/rewardsservice/handlers/customer_reward_handler.py
@@ -2,9 +2,24 @@ import json
 import tornado.web
 from tornado.gen import coroutine
 import re
+from pymongo import DESCENDING
 
 
 class CustomerRewardsHandler(tornado.web.RequestHandler):
+    @coroutine
+    def post(self):
+        email = self.get_email(True)
+        points = self.get_total()
+        customer = self.get_customer_by_email(email, False)
+        if customer:
+            currPoints = customer["rewardPoints"] if customer else 0
+            points += currPoints
+        reward_data = self.get_reward_level(points)
+        formatted_object = self.format_customer_upsert_object(
+            reward_data, email, points)
+        self.replace_customer_data(email, formatted_object)
+        self.set_header('Content-Type', 'application/json')
+        self.write(json.dumps(formatted_object))
 
     @coroutine
     def get(self):
@@ -25,12 +40,53 @@ class CustomerRewardsHandler(tornado.web.RequestHandler):
             raise ValueError(email + ' is not a valid email')
         return email
 
-    def get_customer_by_email(self, email):
+    def get_total(self):
+        total = self.get_argument('total', None, True)
+        regchecktotal = re.match(
+            '(?=.*?\d)?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$', total)
+        if not regchecktotal:
+            raise ValueError(
+                'Total can only be a float number with digits only and up to 2 decimal points.')
+        else:
+            actualtotal = int(float(regchecktotal.groups()[0]))
+        if actualtotal < 0:
+            raise ValueError('Total must be greater than 0')
+        return actualtotal
+
+    def get_customer_by_email(self, email, req=True):
         customer = self.settings["db"].customerRewards.find_one(
             {"email": email}, {"_id": 0})
+        
         return customer
 
     def get_all_customer_reward_data(self):
         customer = list(
             self.settings["db"].customerRewards.find({}, {"_id": 0}))
         return customer
+
+    def replace_customer_data(self, email, customer_object):
+        self.settings["db"].customerRewards.replace_one(
+            {"email": email}, customer_object, upsert=True)
+        return
+
+    def get_reward_level(self, points):
+        floor_points = float(points / 100) 
+        floor_points *= 100
+        if floor_points >= 1000:
+            return list([{}, self.settings["db"].rewards.find_one({'points': 1000}, {"_id": 0})])
+        if floor_points < 100:
+            return list([self.settings["db"].rewards.find_one({'points': 100}, {"_id": 0}), {}])
+        return list(self.settings["db"].rewards.find({'points': {
+            '$in': [floor_points, floor_points+100]}}, sort=[('points', DESCENDING)]))
+
+    def format_customer_upsert_object(self, reward_data, email, points):
+        formatted_customer_object = {
+            "email": email,
+            "rewardPoints": points,
+            "rewardTier": reward_data[1].get("tier") if reward_data[1] else '',
+            "rewardTierName": reward_data[1].get("rewardName") if reward_data[1] else '',
+            "nextRewardTier": reward_data[0].get("tier") if reward_data[0] else '',
+            "nextRewardTierName": reward_data[0].get("rewardName") if reward_data[0] else '',
+            "nextRewardTierProgress": points / reward_data[0].get("points") if reward_data[0].get("points") else "",
+        }
+        return formatted_customer_object
