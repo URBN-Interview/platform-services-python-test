589d9ad98f522990cf72af4d6ba4fd975409dd6a
majegbile <49738529+majegbile@users.noreply.github.com>
2022-07-11 09:52:45 -0500

    Update url_patterns.py

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,10 @@
 from handlers.rewards_handler import RewardsHandler
-
+from handlers.rewards_handler import RewardCalHandler
+from handlers.rewards_handler import RewardDetailHandler
+from handlers.rewards_handler import AllDetailHandler
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/addreward', RewardCalHandler),
+    (r'/getReward', RewardDetailHandler),
+    (r'/getAllReward', AllDetailHandler)
 ]
