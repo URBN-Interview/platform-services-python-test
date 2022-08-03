b7f741866235086fb3b7d64e65db316b3ce502f5
James Kelly <jpkelly095@gmail.com>
2022-08-03 16:27:43 -0400

    Cleanup first endpoint code, use single update with upsert=True

--- source/RewardsService/rewardsservice/handlers/rewards_data_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_data_handler.py
@@ -27,34 +27,24 @@ class RewardsDataHandler(tornado.web.RequestHandler):
         query = {"email_address": email}
         rewards_data = self.db.rewards_data.find_one(query)
         if rewards_data == None:
-            new_tier = self.helper.find_new_tier(points_earned)
-            next_tier = self.helper.find_next_tier(points_earned)
-            next_tier_progress = self.helper.calculate_next_tier_progress(points_earned, new_tier["points"], next_tier["points"])
-
-            self.db.rewards_data.insert({
-                "email_address": email,
-                "reward_points": points_earned,
-                "reward_tier": new_tier["tier"],
-                "reward_tier_name": new_tier["rewardName"],
-                "next_reward_tier": next_tier["tier"],
-                "next_reward_tier_name": next_tier["rewardName"],
-                "next_reward_tier_progress": next_tier_progress
-            })
+            new_points_total = points_earned
         else:
             new_points_total = rewards_data["reward_points"] + points_earned
-            new_tier = self.helper.find_new_tier(new_points_total)
-            next_tier = self.helper.find_next_tier(new_points_total)
-            next_tier_progress = self.helper.calculate_next_tier_progress(new_points_total, new_tier["points"], next_tier["points"])
 
-            updated_values = { "$set": {
-                "reward_points": new_points_total,
-                "reward_tier": new_tier["tier"],
-                "reward_tier_name": new_tier["rewardName"],
-                "next_reward_tier": next_tier["tier"],
-                "next_reward_tier_name": next_tier["rewardName"],
-                "next_reward_tier_progress": next_tier_progress
-            }}
-            self.db.rewards_data.update_one(query, updated_values)
+        new_tier = self.helper.find_new_tier(new_points_total)
+        next_tier = self.helper.find_next_tier(new_points_total)
+        next_tier_progress = self.helper.calculate_next_tier_progress(new_points_total, new_tier["points"], next_tier["points"])
+
+        new_values = { "$set": {
+            "email_address": email,
+            "reward_points": new_points_total,
+            "reward_tier": new_tier["tier"],
+            "reward_tier_name": new_tier["rewardName"],
+            "next_reward_tier": next_tier["tier"],
+            "next_reward_tier_name": next_tier["rewardName"],
+            "next_reward_tier_progress": next_tier_progress
+        }}
+        self.db.rewards_data.update_one(query, new_values, upsert=True)
         
         rewards_data = self.db.rewards_data.find_one(query)
         self.write(json.dumps(rewards_data, default=lambda o: '<not serializable>'))
