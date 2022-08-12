053a630f964473378de29529b7ee0e1f9a75a964
Muammar Johnson <muammarjohnson@Muammars-Mac-mini.local>
2022-08-12 19:32:50 -0400

    first commit

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -13,3 +13,25 @@ class RewardsHandler(tornado.web.RequestHandler):
         db = client["Rewards"]
         rewards = list(db.rewards.find({}, {"_id": 0}))
         self.write(json.dumps(rewards))
+
+class EndPointOne(tornado.web.RequestHandler):
+    """post or put for user
+    1. database should already be created
+    2. get should show form
+    3. post should put data into database"""
+    def get(self):
+        items = ["Item 1", "Item 2", "Item 3"]
+        self.render("endpoint_one.html", title="My title", items=items)
+    
+    def post(self):
+        self.write(self.__class__.__name__)
+
+class EndPointTwo(tornado.web.RequestHandler):
+    """get user info"""
+    def get(self):
+        self.write(self.__class__.__name__)
+
+class EndPointThree(tornado.web.RequestHandler):
+    """Get all user Info"""
+    def get(self):
+        self.write(self.__class__.__name__)
--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,9 @@
-from handlers.rewards_handler import RewardsHandler
+from handlers.rewards_handler import RewardsHandler, EndPointOne, EndPointTwo, EndPointThree
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/endpoint_one', EndPointOne),
+    (r'/endpoint_two', EndPointTwo),
+    (r'/endpoint_three', EndPointThree)
 ]
+
