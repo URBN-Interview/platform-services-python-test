80e0f8dbdb116826f557d0ca75d33d2b04b5fc91
Muammar Johnson <muammarjohnson@Muammars-Mac-mini.local>
2022-08-13 15:13:54 -0400

    enpoint two complete

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -13,7 +13,6 @@ class RewardsHandler(tornado.web.RequestHandler):
         db = client["Rewards"]
         rewards = list(db.rewards.find({}, {"_id": 0}))
         rewards = db.rewards.find_one({"points": 200})
-        # self.write(str(rewards['tier']))
         self.write(str(rewards))
         
 
@@ -26,16 +25,15 @@ class EndPointOne(tornado.web.RequestHandler):
         user_info_db = client["user_info"]
         rewards_db = client["Rewards"]
         email = self.get_argument("email")
-        saved_user_info = user_info_db.users.find_one({"email":email})
+        saved_user_info = user_info_db.user_info.find_one({"email":email})
         if saved_user_info:
             tier, points = self.calculate_points(
                 int(self.get_argument("points")),
                 int(saved_user_info.get('points'))
-            )
-            user_info_db.users.update_one({"email":email},{"$set":{"points":points}})   
+            ) 
         else:
             tier, points = self.calculate_points(int(self.get_argument("points")))
-            user_info_db.users.insert({"email":email, "points":points})
+            user_info_db.user_info.insert({"email":email, "points":points})
         tier_progress = (points%100)/100
         if points< 100:
             reward = {'tier': 'None','rewardName': 'None'}
@@ -48,7 +46,7 @@ class EndPointOne(tornado.web.RequestHandler):
             next_reward = {'tier': 'None','rewardName': 'None'}
             tier_progress = "Maxed Out"
         user_reward_dic = {
-            "email_address" : email,
+            "email" : email,
             "reward_points" : points,
             "reward_tier" : reward.get('tier'),
             "reward_tier_name" : reward.get('rewardName'),
@@ -56,7 +54,8 @@ class EndPointOne(tornado.web.RequestHandler):
             "next_reward_tier_name" : next_reward.get('rewardName'),
             "next_reward_tier_progress" : tier_progress
             }
-        self.render("endpoint_one_post.html", title="post", spot=user_reward_dic)
+        self.update_user_info(user_info_db, **user_reward_dic)
+        self.render("data_display.html", title="post", spot=user_reward_dic)
 
     def calculate_points(self,new_points, old_points=0):
         total_points = new_points+old_points
@@ -65,11 +64,40 @@ class EndPointOne(tornado.web.RequestHandler):
         else:
             tier = int(math.ceil(total_points / 100.0)) * 100 
             return (tier if tier <1000 else 1000, total_points)
+    
+    def update_user_info(
+            self,
+            user_info_db,
+            email, 
+            reward_points, 
+            reward_tier, 
+            reward_tier_name, 
+            next_reward_tier, 
+            next_reward_tier_name, 
+            next_reward_tier_progress):
+        user_info_db.user_info.update_one({"email":email},
+        {"$set":{
+            "reward_points":reward_points,
+            "reward_tier":reward_tier,
+            "reward_tier_name":reward_tier_name, 
+            "next_reward_tier": next_reward_tier, 
+            "next_reward_tier_name": next_reward_tier_name, 
+            "next_reward_tier_progress": next_reward_tier_progress}}) 
 
 class EndPointTwo(tornado.web.RequestHandler):
     """get user info"""
     def get(self):
-        self.write(self.__class__.__name__)
+        self.render("endpoint_two.html", title="post",)
+    def post(self):
+        client = MongoClient("mongodb", 27017)
+        user_info_db = client["user_info"]
+        email = self.get_argument("email")
+        user_info = user_info_db.user_info.find_one({"email":email})
+        if user_info:
+            self.render("data_display.html", title="post", spot=user_info)
+        else:
+            self.write("that email does not exist in the database")
+
 
 class EndPointThree(tornado.web.RequestHandler):
     """Get all user Info"""
