cda7c9d18d624af2de17161d1ac259c39a1710bf
Eguakun <meguakun@my.tnstate.edu>
2022-09-02 12:05:30 -0500

    quick change. no need to import from base handler

--- source/RewardsService/rewardsservice/error_handling/error_handling.py
+++ source/RewardsService/rewardsservice/error_handling/error_handling.py
@@ -1,18 +1,30 @@
+from lib2to3.pytree import Base
 import tornado.web
-
+from email_validator import validate_email, EmailNotValidError
 
 class BaseHandler(tornado.web.RequestHandler):
     def write_error(self, status_code, **kwargs):
         if status_code in [403, 404, 500, 503]:
-            self.write('Error %s' % status_code)
+            self.write('ERROR %s' % status_code)
         else:
-            self.write('Error. Please try again.')
-
-
-class ErrorHandler(tornado.web.ErrorHandler, BaseHandler):
-    pass
+            self.write('ERROR. Please try again.')
 
 
 class MainHandler(BaseHandler):
     def get(self):
-        self.write('Error. Please try again.')
+        self.write('ERROR. Please try again.')
+
+class ErrorHandler(tornado.web.ErrorHandler, BaseHandler):
+    pass
+
+class EmailErrorHandler():
+
+      def validate_email_address(self, email):
+        try:
+            validation = validate_email(email)  
+            email = validation["email"]
+            return email
+        except EmailNotValidError as exc:
+            return [str(exc)]
+
+            
