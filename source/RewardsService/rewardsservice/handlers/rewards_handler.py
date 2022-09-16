a26917f9785a7c7ea95df73e335784ba21469117
CharanDhanireddy <drcharaniitg@gmail.com>
2022-09-15 21:39:30 -0400

    Adds singleton DB connection

--- source/RewardsService/rewardsservice/handlers/rewards_handler.py
+++ source/RewardsService/rewardsservice/handlers/rewards_handler.py
@@ -1,15 +1,15 @@
 import json
 import tornado.web
 
-from pymongo import MongoClient
 from tornado.gen import coroutine
+from util.db_connection import DBConnection
 
 
 class RewardsHandler(tornado.web.RequestHandler):
 
     @coroutine
     def get(self):
-        client = MongoClient("mongodb", 27017)
+        client = DBConnection.get_client()
         db = client["Rewards"]
         rewards = list(db.rewards.find({}, {"_id": 0}))
         self.write(json.dumps(rewards))
