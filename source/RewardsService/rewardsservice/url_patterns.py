1277581dd06c4dec65466e00e836d0238bcf9172
sonal744 <115610370+sonal744@users.noreply.github.com>
2022-10-14 19:25:34 -0400

    Adding customer urls

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,9 @@
 from handlers.rewards_handler import RewardsHandler
-
+from handlers.customers_handler import CustomersHandler
+from handlers.customer_handler import CustomerHandler
+from db import Database
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/customers', CustomersHandler, dict(db=Database())),
+    (r'/customer', CustomerHandler, dict(db=Database())),
 ]
