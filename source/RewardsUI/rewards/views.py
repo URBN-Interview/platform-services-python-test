e46a8fccf37b54bfb3f47025ec492ede9427ce68
sonal744 <115610370+sonal744@users.noreply.github.com>
2022-10-14 19:33:24 -0400

    Rewards UI display formatting

--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -3,7 +3,7 @@ import logging
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
 
-from rewards.clients.rewards_service_client import RewardsServiceClient
+from clients.rewards_service_client import RewardsServiceClient
 
 
 class RewardsView(TemplateView):
@@ -19,8 +19,23 @@ class RewardsView(TemplateView):
         rewards_data = self.rewards_service_client.get_rewards()
         context['rewards_data'] = rewards_data
 
-        return TemplateResponse(
-            request,
-            self.template_name,
-            context
-        )
+        email_address = request.GET.get('email_address')
+        if email_address:
+            customers_data = [self.rewards_service_client.get_customer(email_address)]
+        else:
+            customers_data = self.rewards_service_client.get_customers()
+
+        context['customers_data'] = self.format_customers_data(customers_data)
+
+        def customers_data_formatiing(self, customers_data):
+            for customer in customers_data:
+                if customer is None:
+                    continue
+
+                customer['next_reward_tier_progress'] = "{0:.0%}".format(customer['next_reward_tier_progress'])
+
+                for key, value in customer.items():
+                    if not value:
+                        customer[key] = 'N/A'
+
+            return customers_data
