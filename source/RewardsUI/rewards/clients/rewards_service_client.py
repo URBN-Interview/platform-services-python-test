5b3970231bd0659a95fc66c2bc5b4a7fb7dbabaa
sonal744 <115610370+sonal744@users.noreply.github.com>
2022-10-14 19:30:05 -0400

    setting up of customer rewards UI

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -5,7 +5,18 @@ class RewardsServiceClient:
 
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
+        self.customers_url = "http://rewardsservice:7050/customers"
+        self.customer_url = "http://rewardsservice:7050/customer"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
+        return response.json()
+
+    def get_customers(self):
+        response = requests.get(self.customers_url)
+        return response.json()
+
+    def get_customer(self, email_address):
+        url = self.customer_url + '?email_address={}'.format(email_address)
+        response = requests.get(url)
         return response.json()
