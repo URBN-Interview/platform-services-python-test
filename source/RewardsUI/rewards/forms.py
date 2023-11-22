2e0234d83d3d7fc0e7a48fda27509acfb3eb8157
Colin Herbert <colin.herbert.13@gmail.com>
2023-11-22 11:25:56 -0700

    [Commit-11] Finished styling for webpage, added some error handling for form submission if an HTTP error occurs or if an email doesn't exist in the search form

--- source/RewardsUI/rewards/forms.py
+++ source/RewardsUI/rewards/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 
 class OrderForm(forms.Form):
-    email = forms.CharField(max_length=200, label="Enter email address")
-    order_total = forms.FloatField(label="Enter order total", min_value=0.0)
+    email = forms.CharField(max_length=200, label=mark_safe("Enter email address: "), label_suffix="")
+    order_total = forms.FloatField(label=mark_safe("<br/><br/>Enter order total: "), min_value=0.0, label_suffix="")
 
 
 class UserFilterForm(forms.Form):
-    email = forms.CharField(max_length=200, label="Email address")
+    email = forms.CharField(max_length=200, label="Email address: ", label_suffix="")
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -2,7 +2,7 @@ import logging
 
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseServerError
 
 from rewards.clients.rewards_service_client import RewardsServiceClient
 from rewards.forms import OrderForm, UserFilterForm
@@ -18,8 +18,12 @@ class RewardsView(TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
 
-        rewards_data = self.rewards_service_client.get_rewards()
-        customers_data = self.rewards_service_client.get_customers()
+        try:
+            rewards_data = self.rewards_service_client.get_rewards()
+            customers_data = self.rewards_service_client.get_customers()
+        except HttpResponseServerError as e:
+            rewards_data = []
+            customers_data = []
 
         context['rewards_data'] = rewards_data
         context['customers_data'] = customers_data
@@ -29,7 +33,12 @@ class RewardsView(TemplateView):
         filter_form = UserFilterForm(request.GET)
         if filter_form.is_valid():
             filter_email = filter_form.cleaned_data["email"]
-            context['customers_data'] = self.rewards_service_client.get_filtered_customer(filter_email)
+            try:
+                customer_filter = self.rewards_service_client.get_filtered_customer(filter_email)
+                if len(customer_filter) > 0:
+                    context['customers_data'] = customer_filter
+            except HttpResponseServerError as e:
+                self.logger.error('No customer found with email: ' + filter_email + ', aborting...')
 
         for customer in context['customers_data']:
             customer['nextTierProgress'] = int(customer['nextTierProgress'] * 100)
