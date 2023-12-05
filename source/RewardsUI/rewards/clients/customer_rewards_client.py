ac0b2bad1f6fe390a026ee86943c4f80874a14fc
Brady Ketelsen <brady.ketelsen@gmail.com>
2023-12-05 16:06:05 -0500

    add ability to search for single customer with email

--- source/RewardsUI/rewards/clients/customer_rewards_client.py
+++ source/RewardsUI/rewards/clients/customer_rewards_client.py
@@ -1,15 +1,21 @@
 import requests
-
+import json
 
 class CustomerRewardsClient:
 
     def __init__(self):
-        pass
+        self.customer_url = "http://rewardsservice:7050/customers"
 
-    # def get_customer(self, email):
-    #     response = requests.get(f"http://rewardsservice:7050/customers/{email}")
-    #     return response.json()
+    def get_customer(self, email):
+        response = requests.get(self.customer_url + "/" + email)
+        return response.json()
 
     def get_customer_list(self):
         response = requests.get("http://rewardsservice:7050/customers")
         return response.json()
+
+    def update_customer_rewards(self, email, order):
+        request_url = "http://rewardsservice:7050/customers/" + email
+        body = json.dumps({"order": float(order)})
+        response = requests.get(request_url, data=body)
+        print(response)
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
 from rewards.clients.rewards_service_client import RewardsServiceClient
 from rewards.clients.customer_rewards_client import CustomerRewardsClient
@@ -16,11 +17,35 @@ class RewardsView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-
         rewards_data = self.rewards_service_client.get_rewards()
         customer_data = self.customer_rewards_client.get_customer_list()
         context['rewards_data'] = rewards_data
         context['customer_data'] = customer_data
+        query_dict = request.GET
+        email = query_dict.get("email")
+        if email is not None:
+            customer_rewards = self.customer_rewards_client.get_customer(email)
+            context['customer_rewards'] = customer_rewards
+
+        return TemplateResponse(
+            request,
+            self.template_name,
+            context
+        )
+
+    def put(self, request, **kwargs):
+        if request.method == "POST":
+            email = request.POST.get("email")
+            order = request.POST.get("order")
+            response = self.customer_rewards_client.update_customer_rewards(email, order)
+            if response:
+                context = {}
+                rewards_data = self.rewards_service_client.get_rewards()
+                customer_data = self.customer_rewards_client.get_customer_list()
+                context['rewards_data'] = rewards_data
+                context['customer_data'] = customer_data
+            else:
+                return HttpResponse("PUT was unsuccessful")
 
         return TemplateResponse(
             request,
