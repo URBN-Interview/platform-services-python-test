b2b88b24b074f2b0a0826c66e7b6e628a69ed388
Breanna Steinfeldt <breanna.steinfeldt@gmail.com>
2023-12-09 16:49:26 -0500

    figured out how to query db

--- source/RewardsService/rewardsservice/handlers/order_handler.py
+++ source/RewardsService/rewardsservice/handlers/order_handler.py
@@ -1,15 +1,15 @@
+from decimal import Decimal
 import json
 from tornado import web, escape
 
 from pymongo import MongoClient
 from tornado.gen import coroutine
 
-
+CLIENT = MongoClient("mongodb", 27017)
 
 class OrderHandler(web.RequestHandler):    
     def post(self):
-        client = MongoClient("mongodb", 27017)
-        db = client["Rewards"]
+        db = CLIENT["Rewards"]
         body = escape.json_decode(self.request.body)
         customer_email = body["data"]["customer_email"]
         order_total = body["data"]["order_total"]
@@ -17,5 +17,10 @@ class OrderHandler(web.RequestHandler):
             "email_address": customer_email, 
             "order_total": order_total
         }
-        db.rewards.insert_one(order)
+        self.rewards_calculation(order)
         self.write("added an order")
+
+    def rewards_calculation(self, order):
+        db = CLIENT["Rewards"]
+        order_total = int(Decimal(order["order_total"]))
+        print(db.rewards.find_one({"points": order_total}))
