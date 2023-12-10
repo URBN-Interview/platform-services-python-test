c05ab615ac7bb07d9d5e2c0e069c184a64688765
Breanna Steinfeldt <breanna.steinfeldt@gmail.com>
2023-12-09 19:05:19 -0500

    solved percent to next tier, user rewards data model, switching to story branch

--- source/RewardsService/rewardsservice/handlers/order_handler.py
+++ source/RewardsService/rewardsservice/handlers/order_handler.py
@@ -18,14 +18,34 @@ class OrderHandler(web.RequestHandler):
             "email_address": customer_email, 
             "order_total": order_total
         }
-        self.rewards_calculation(order)
-        self.write("added an order")
+        rewards_data = self.rewards_calculation(order)
+        db.rewards.insert_one(rewards_data)
+        self.write("new reward data added")
+
 
     def rewards_calculation(self, order):
+        #catch when order is $1000
         db = CLIENT["Rewards"]
         order_total = int(Decimal(order["order_total"]))
-        print(db.rewards.find_one({"points": self.round_down(order_total)}))
+        current_reward_item = db.rewards.find_one({"points": self.round_down(order_total)})
+        next_reward_item = db.rewards.find_one({"points": current_reward_item["points"]+100})
+        rewards_data = {
+            "email_address": order["email_address"], 
+            "reward_points": order_total, 
+            "reward_tier": current_reward_item["tier"], 
+            "reward_tier_name": current_reward_item["rewardName"], 
+            "next_reward_tier": next_reward_item["tier"], 
+            "next_reward_tier_name": next_reward_item["rewardName"], 
+            "next_reward_tier_progress": self.percent_to_next_tier(order_total, next_reward_item["points"])
+        }
+        return rewards_data
+
 
     def round_down(self, num): 
         rounded_number = math.floor(num/100) * 100 
         return rounded_number 
+    
+
+    def percent_to_next_tier(self, current_num, next_num):
+        percent = (next_num - current_num)/100
+        return percent
