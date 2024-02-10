9a16026b9230b5daa843a4699bb7e27bfae4836d
sweta-sv <sweta.k@xnara.com>
2024-02-10 22:34:20 +0530

    Updated url_patterns.py with new handler endpoint

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,6 @@
-from handlers.rewards_handler import RewardsHandler
+from handlers.rewards_handler import RewardsHandler, RewardPointsHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/rewardpoint', RewardPointsHandler,)
 ]
