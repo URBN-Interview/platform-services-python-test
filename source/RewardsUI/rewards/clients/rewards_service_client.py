d23c285f9a3871ef070d4a9bfaeb9653c1e6964e
sweta-sv <sweta.k@xnara.com>
2024-02-10 22:35:28 +0530

    Updated rewards_service_client.py file with new addition RewardsServiceClient.add_orders()

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -5,7 +5,13 @@ class RewardsServiceClient:
 
     def __init__(self):
         self.rewards_url = "http://rewardsservice:7050/rewards"
+        self.add_orders_url = "http://rewardsservice:7050/rewardpoint"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
         return response.json()
+    
+    def add_orders(self, email, order_total):
+        response = requests.post(self.add_orders_url, data={"email": email , "order_total": order_total})
+        return response.json()
+
