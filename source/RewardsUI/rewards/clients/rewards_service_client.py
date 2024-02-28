0c343b494e9993c348697dc2d893963a3ecaf341
sheshantsingh <ssheshant@gmail.com>
2024-02-28 22:35:13 +0530

    added all the fixes needed for frontend

--- source/RewardsUI/rewards/clients/rewards_service_client.py
+++ source/RewardsUI/rewards/clients/rewards_service_client.py
@@ -4,8 +4,22 @@ import requests
 class RewardsServiceClient:
 
     def __init__(self):
-        self.rewards_url = "http://rewardsservice:7050/rewards"
+        self.rewards_url = "http://localhost:7050/rewards"
+        self.user_url = "http://localhost:7050/rewards/users"
+        self.order_url = "http://localhost:7050/rewards/order"
 
     def get_rewards(self):
         response = requests.get(self.rewards_url)
         return response.json()
+
+    def add_rewards(self, email_address, order_total):
+        response = requests.post(self.order_url, json=dict(
+            email_address=email_address,
+            order_total=order_total
+        ))
+        return response.json()
+
+    def get_users(self, email_address=""):
+        response = requests.get(self.user_url, params=dict(email_address=email_address))
+        return response.json()
+
--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -12,12 +12,37 @@ class RewardsView(TemplateView):
     def __init__(self, logger=logging.getLogger(__name__), rewards_service_client=RewardsServiceClient()):
         self.logger = logger
         self.rewards_service_client = rewards_service_client
+        self.rewards_data = []
+
+    def get_rewards_data(self):
+        if not self.rewards_data:
+            self.rewards_data = self.rewards_service_client.get_rewards()
+        return self.rewards_data
 
     def get(self, request, *args, **kwargs):
+        email_address = request.GET.get("email", "")
         context = self.get_context_data(**kwargs)
 
-        rewards_data = self.rewards_service_client.get_rewards()
+        rewards_data = self.get_rewards_data()
+        users_data = self.rewards_service_client.get_users(email_address=email_address)
         context['rewards_data'] = rewards_data
+        context['users_data'] = users_data
+
+        return TemplateResponse(
+            request,
+            self.template_name,
+            context
+        )
+
+    def post(self, request, *args, **kwargs):
+        email_address = request.POST.get("email_address", "")
+        order_total = request.POST.get("order_total", 0)
+        data = self.rewards_service_client.add_rewards(email_address=email_address, order_total=order_total)
+        context = self.get_context_data(**kwargs)
+        rewards_data = self.get_rewards_data()
+        users_data = self.rewards_service_client.get_users()
+        context['rewards_data'] = rewards_data
+        context['users_data'] = users_data
 
         return TemplateResponse(
             request,
