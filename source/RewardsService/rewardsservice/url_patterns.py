a4ce0d86cf612681b786f7b0d6be68d08ae55634
gajanankathar <gajanan.kathar123@gmail.com>
2024-03-03 03:28:22 +0530

    added customer endpoint

--- source/RewardsService/rewardsservice/url_patterns.py
+++ source/RewardsService/rewardsservice/url_patterns.py
@@ -1,5 +1,7 @@
+from handlers.customer_rewards_handler import CustomerRewardsHandler
 from handlers.rewards_handler import RewardsHandler
 
 url_patterns = [
     (r'/rewards', RewardsHandler),
+    (r'/customers', CustomerRewardsHandler),
 ]
