20122b21713bbcd04287b49ad12a8cca4e407882
gajanankathar <gajanan.kathar123@gmail.com>
2024-03-03 17:56:22 +0530

    added view logic to add new order

--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -1,8 +1,11 @@
 import logging
 
+from django.urls import reverse
+from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
 
+from rewards.forms import AddRewardsForm
 from rewards.clients.rewards_service_client import RewardsServiceClient
 
 
@@ -18,9 +21,20 @@ class RewardsView(TemplateView):
 
         rewards_data = self.rewards_service_client.get_rewards()
         context['rewards_data'] = rewards_data
-
         return TemplateResponse(
             request,
             self.template_name,
             context
         )
+
+    def post(self, request, *args, **kwargs):
+        form = AddRewardsForm(request.POST)
+        if form.is_valid():
+            data = {
+                "emailId": form.cleaned_data["email"],
+                "orderTotal": form.cleaned_data["total"],
+            }
+            data = self.rewards_service_client.add_order(data)
+            return redirect(reverse("rewards"))
+
+        return redirect(reverse("rewards"), order_form=form.cleaned_data)
