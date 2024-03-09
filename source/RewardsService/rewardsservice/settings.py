c1ea5abdd86ad3c023fc84f55348d7f1bda0ea91
vrushabh deshmukh <itsvrushabh@gmail.com>
2024-03-09 16:24:50 +0530

    update the api and other details

--- source/RewardsService/rewardsservice/settings.py
+++ source/RewardsService/rewardsservice/settings.py
@@ -1,18 +1,24 @@
 import os
 import tornado.template
-
 from tornado.options import define
 
+# Define a lambda function to construct file paths
 path = lambda root, *a: os.path.join(root, *a)
+
+# Get the absolute path of the current directory
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Define the paths for static files and templates
 STATIC_ROOT = path(ROOT, 'static')
 TEMPLATE_ROOT = path(ROOT, 'templates')
 
-define("port", default=7050, help="run on the given port", type=int)
+# Define a Tornado option for the port
+define("port", default=7050, help="Run on the given port", type=int)
 
+# Configuration settings for the Tornado application
 settings = {
-    'debug': True,
-    'static_path': STATIC_ROOT,
-    'template_loader': tornado.template.Loader(TEMPLATE_ROOT)
+    'debug': True,  # Enable debug mode for better error messages
+    'static_path': STATIC_ROOT,  # Path to static files
+    'template_loader': tornado.template.Loader(TEMPLATE_ROOT)  # Template loader for rendering HTML templates
 }
+
